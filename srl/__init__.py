7e81789f4abcd0262384387cd9d956de59c2fd18
poco_cpp <pocopococpp198@gmail.com>
2022-06-11 18:37:39 +0900

    順番を同じに修正

--- srl/__init__.py
+++ srl/__init__.py
@@ -1,4 +1,4 @@
-from . import base, envs, rl, runner, utils
+from . import base, envs, rl, runner, test, utils
 from .version import VERSION as __version__  # noqa F402
 
-__all__ = ["rl", "runner", "envs", "base", "utils", "test"]
+__all__ = ["base", "envs", "rl", "runner", "test", "utils"]

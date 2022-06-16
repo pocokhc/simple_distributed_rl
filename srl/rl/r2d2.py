6cb42314752fda9c9c02d5f46692b935af77a9e1
poco_cpp <pocopococpp198@gmail.com>
2022-06-16 20:25:31 +0900

    論文を読んで間違っている点を修正

--- srl/rl/r2d2.py
+++ srl/rl/r2d2.py
@@ -24,6 +24,9 @@ from srl.rl.functions.model import ImageLayerType, create_input_layers_lstm_stat
 from tensorflow.keras import layers as kl
 
 """
+・Paper
+https://openreview.net/forum?id=r1lyTjAqYX
+
 DQN
     window_length          : -
     Fixed Target Q-Network : o
@@ -37,17 +40,16 @@ Rainbow
     Double DQN                  : o (config selection)
     Priority Experience Replay  : o (config selection)
     Dueling Network             : o (config selection)
-    Multi-Step learning(retrace): o (config selection)
+    Multi-Step learning         : x
     Noisy Network               : x
     Categorical DQN             : x
 Recurrent Replay Distributed DQN(R2D2)
     LSTM                     : o
-    Value function rescaling : o
+    Value function rescaling : o (config selection)
+Never Give Up(NGU)
+    Retrace          : o (config selection)
 Other
     invalid_actions : o
-
-Implementation plan list
- - Calculation of priority on the worker side
 
 """
 
@@ -68,18 +70,24 @@ class Config(DiscreteActionConfig):
     hidden_layer_sizes: Tuple[int, ...] = (512,)
     activation: str = "relu"
     image_layer_type: ImageLayerType = ImageLayerType.DQN
-    burnin: int = 40
 
-    gamma: float = 0.99  # 割引率
-    lr: float = 0.001  # 学習率
+    # lstm
+    burnin: int = 5
+    sequence_length: int = 5
+
+    gamma: float = 0.997
+    lr: float = 0.001
     batch_size: int = 32
     target_model_update_interval: int = 1000
 
     # double dqn
     enable_double_dqn: bool = True
 
+    # rescale
+    enable_rescale: bool = False
+
     # retrace
-    multisteps: int = 1
+    enable_retrace: bool = True
     retrace_h: float = 1.0
 
     # DuelingNetwork
@@ -94,10 +102,40 @@ class Config(DiscreteActionConfig):
     memory_beta_initial: float = 1.0
     memory_beta_steps: int = 1_000_000
 
+    # other
     dummy_state_val: float = 0.0
 
     def set_config_by_actor(self, actor_num: int, actor_id: int) -> None:
         self.epsilon = create_epsilon_list(actor_num, epsilon=self.actor_epsilon, alpha=self.actor_alpha)[actor_id]
+
+    # 論文のハイパーパラメーター
+    def set_atari_config(self):
+
+        # model
+        self.lstm_units = 512
+        self.hidden_layer_sizes = (512,)
+        self.image_layer_type = ImageLayerType.DQN
+
+        # lstm
+        self.burnin = 40
+        self.sequence_length = 80
+
+        self.gamma = 0.997
+        self.lr = 0.0001
+        self.batch_size = 64
+        self.target_model_update_interval = 2500
+
+        self.enable_double_dqn = True
+        self.enable_dueling_network = True
+        self.enable_rescale = True
+        self.enable_retrace = False
+
+        self.capacity = 1_000_000
+        self.memory_name: str = "ProportionalMemory"
+        self.memory_warmup_size: int = 80_000
+        self.memory_alpha: float = 0.9
+        self.memory_beta_initial: float = 0.6
+        self.memory_beta_steps: int = 1_000_000
 
     def __post_init__(self):
         super().__init__()
@@ -113,7 +151,7 @@ class Config(DiscreteActionConfig):
     def assert_params(self) -> None:
         super().assert_params()
         assert self.burnin >= 0
-        assert self.multisteps >= 1
+        assert self.sequence_length >= 1
         assert self.memory_warmup_size < self.capacity
         assert self.batch_size < self.memory_warmup_size
 
@@ -150,8 +188,8 @@ class RemoteMemory(PriorityExperienceReplay):
 class _QNetwork(keras.Model):
     def __init__(self, config: Config):
         super().__init__()
-        # input_shape: (batch_size, input_sequence(timestamps), observation_shape)
 
+        # input_shape: (batch_size, input_sequence(timestamps), observation_shape)
         # timestamps=1(stateful)
         in_state, c = create_input_layers_lstm_stateful(
             config.batch_size,
@@ -256,8 +294,7 @@ class Trainer(RLTrainer):
 
         indices, batchs, weights = self.remote_memory.sample(self.train_count, self.config.batch_size)
         td_errors, loss = self._train_on_batchs(batchs, weights)
-        priorities = np.abs(td_errors) + 0.0001
-        self.remote_memory.update(indices, batchs, priorities)
+        self.remote_memory.update(indices, batchs, td_errors)
 
         # targetと同期
         if self.train_count % self.config.target_model_update_interval == 0:
@@ -265,36 +302,25 @@ class Trainer(RLTrainer):
             self.sync_count += 1
 
         self.train_count += 1
-        return {
-            "loss": loss,
-            "priority": np.mean(priorities),
-            "sync": self.sync_count,
-        }
+        return {"loss": loss, "sync": self.sync_count}
 
     def _train_on_batchs(self, batchs, weights):
 
-        # burnin=2
-        # multisteps=3
-        # states  [0,1,2,3,4,5,6]
-        # burnin   o o
-        # state        o,o
-        # n_state1       o,o
-        # n_state2         o,o
-        # n_state3           o,o
-
         # (batch, dict[x], multisteps) -> (multisteps, batch, x)
         states_list = []
-        for i in range(self.config.multisteps + 1):
+        for i in range(self.config.sequence_length + 1):
             states_list.append(np.asarray([[b["states"][i]] for b in batchs]))
         actions_list = []
         mu_probs_list = []
         rewards_list = []
         dones_list = []
-        for i in range(self.config.multisteps):
+        next_invalid_actions_batchs_list = []
+        for i in range(self.config.sequence_length):
             actions_list.append([b["actions"][i] for b in batchs])
             rewards_list.append([b["rewards"][i] for b in batchs])
             mu_probs_list.append([b["probs"][i] for b in batchs])
             dones_list.append([b["dones"][i] for b in batchs])
+            next_invalid_actions_batchs_list.append([b["invalid_actions"][i] for b in batchs])
 
         # hidden_states
         states_h = []
@@ -303,11 +329,13 @@ class Trainer(RLTrainer):
             states_h.append(b["hidden_states"][0])
             states_c.append(b["hidden_states"][1])
         hidden_states = [np.asarray(states_h), np.asarray(states_c)]
+        hidden_states_t = [np.asarray(states_h), np.asarray(states_c)]
 
-        # burnin
+        # burn-in
         for i in range(self.config.burnin):
             burnin_state = np.asarray([[b["burnin_states"][i]] for b in batchs])
             _, hidden_states = self.parameter.q_online(burnin_state, hidden_states)
+            _, hidden_states_t = self.parameter.q_target(burnin_state, hidden_states_t)
 
         _, _, td_error, _, loss = self._train_steps(
             states_list,
@@ -315,8 +343,10 @@ class Trainer(RLTrainer):
             mu_probs_list,
             rewards_list,
             dones_list,
+            next_invalid_actions_batchs_list,
             weights,
             hidden_states,
+            hidden_states_t,
             0,
         )
         return td_error, loss
@@ -329,74 +359,92 @@ class Trainer(RLTrainer):
         mu_probs_list,
         rewards_list,
         dones_list,
+        next_invalid_actions_batchs_list,
         weights,
         hidden_states,
+        hidden_states_t,
         idx,
     ):
 
         # 最後
-        if idx == self.config.multisteps:
+        if idx == self.config.sequence_length:
             n_states = states_list[idx]
             n_q, _ = self.parameter.q_online(n_states, hidden_states)
-            n_q_target, _ = self.parameter.q_target(n_states, hidden_states)
+            n_q_target, _ = self.parameter.q_target(n_states, hidden_states_t)
             n_q = tf.stop_gradient(n_q).numpy()
             n_q_target = tf.stop_gradient(n_q_target).numpy()
-            return n_q, n_q_target, np.zeros(self.config.batch_size), 1.0, 0
+            # q, target_q, td_error, retrace, loss
+            return n_q, n_q_target, 0.0, 1.0, 0.0
 
         states = states_list[idx]
-        n_states = states_list[idx + 1]
         actions = actions_list[idx]
-        dones = dones_list[idx]
-        rewards = rewards_list[idx]
-        mu_probs = mu_probs_list[idx]
+        next_invalid_actions_batchs = next_invalid_actions_batchs_list[idx]
 
-        q_target, _ = self.parameter.q_target(states, hidden_states)
+        # return用にq_targetを計算
+        q_target, n_hidden_states_t = self.parameter.q_target(states, hidden_states_t)
         q_target = tf.stop_gradient(q_target).numpy()
+
+        # --- 勾配 + targetQを計算
         with tf.GradientTape() as tape:
             q, n_hidden_states = self.parameter.q_online(states, hidden_states)
 
-            n_q, n_q_target, n_td_error, retrace, _ = self._train_steps(
+            # 次のQ値を取得
+            n_q, n_q_target, n_td_error, retrace, n_loss = self._train_steps(
                 states_list,
                 actions_list,
                 mu_probs_list,
                 rewards_list,
                 dones_list,
+                next_invalid_actions_batchs_list,
                 weights,
                 n_hidden_states,
+                n_hidden_states_t,
                 idx + 1,
             )
 
-            target_q = []
+            # targetQを計算
+            target_q = np.zeros(self.config.batch_size)
             for i in range(self.config.batch_size):
-                if dones[i]:
-                    gain = rewards[i]
+                reward = rewards_list[idx][i]
+                done = dones_list[idx][i]
+                next_invalid_actions = next_invalid_actions_batchs[i]
+
+                if done:
+                    gain = reward
                 else:
                     # DoubleDQN: indexはonlineQから選び、値はtargetQを選ぶ
                     if self.config.enable_double_dqn:
+                        n_q[i] = [(-np.inf if a in next_invalid_actions else v) for a, v in enumerate(n_q[i])]
                         n_act_idx = np.argmax(n_q[i])
                     else:
+                        n_q_target[i] = [
+                            (-np.inf if a in next_invalid_actions else v) for a, v in enumerate(n_q_target[i])
+                        ]
                         n_act_idx = np.argmax(n_q_target[i])
                     maxq = n_q_target[i][n_act_idx]
-                    maxq = inverse_rescaling(maxq)
-                    gain = rewards[i] + self.config.gamma * maxq
-                gain = rescaling(gain)
-                target_q.append(gain)
-            target_q = np.asarray(target_q)
+                    if self.config.enable_rescale:
+                        maxq = inverse_rescaling(maxq)
+                    gain = reward + self.config.gamma * maxq
+                if self.config.enable_rescale:
+                    gain = rescaling(gain)
+                target_q[i] = gain
 
-            # retrace
-            _retrace = []
-            for i in range(self.config.batch_size):
-                pi_probs = calc_epsilon_greedy_probs(
-                    n_q[i],
-                    [a for a in range(self.config.nb_actions)],
-                    0.0,
-                    self.config.nb_actions,
-                )
-                r = self.config.retrace_h * np.minimum(1, pi_probs[actions[i]] / mu_probs[i])
-                _retrace.append(r)
-            retrace *= np.asarray(_retrace)
+            if self.config.enable_retrace:
+                _retrace = np.zeros(self.config.batch_size)
+                for i in range(self.config.batch_size):
+                    action = actions_list[idx][i]
+                    mu_prob = mu_probs_list[idx][i]
+                    pi_probs = calc_epsilon_greedy_probs(
+                        n_q[i],
+                        next_invalid_actions_batchs[i],
+                        0.0,
+                        self.config.nb_actions,
+                    )
+                    pi_prob = pi_probs[action]
+                    _retrace[i] = self.config.retrace_h * np.minimum(1, pi_prob / mu_prob)
 
-            target_q += self.config.gamma * retrace * n_td_error
+                retrace *= _retrace
+                target_q += self.config.gamma * retrace * n_td_error
 
             # 現在選んだアクションのQ値
             action_onehot = tf.one_hot(actions, self.config.nb_actions)
@@ -406,10 +454,14 @@ class Trainer(RLTrainer):
 
         grads = tape.gradient(loss, self.parameter.q_online.trainable_variables)
         self.optimizer.apply_gradients(zip(grads, self.parameter.q_online.trainable_variables))
-
-        n_td_error = (target_q - q_onehot).numpy() + self.config.gamma * retrace * n_td_error
+        # --- 勾配計算ここまで
         q = tf.stop_gradient(q).numpy()
-        return q, q_target, n_td_error, retrace, loss.numpy()
+
+        if idx == 0 or self.config.enable_retrace:
+            td_error = (target_q - q_onehot).numpy() + self.config.gamma * retrace * n_td_error
+        else:
+            td_error = 0
+        return q, q_target, td_error, retrace, (loss.numpy() + n_loss) / 2
 
 
 # ------------------------------------------------------
@@ -422,27 +474,45 @@ class Worker(DiscreteActionWorker):
         self.parameter = cast(Parameter, self.parameter)
         self.remote_memory = cast(RemoteMemory, self.remote_memory)
 
-        self.dummy_state = np.full(self.config.observation_shape, self.config.dummy_state_val, dtype=np.float32)
-        self.invalid_action_reward = -1
+        self.dummy_state = np.full(self.config.observation_shape, self.config.dummy_state_val, dtype=float)
 
     def call_on_reset(self, state: np.ndarray, invalid_actions: List[int]) -> None:
-        self.recent_states = [self.dummy_state for _ in range(self.config.burnin + self.config.multisteps + 1)]
-        self.recent_actions = [random.randint(0, self.config.nb_actions - 1) for _ in range(self.config.multisteps)]
-        self.recent_probs = [1.0 / self.config.nb_actions for _ in range(self.config.multisteps)]
-        self.recent_rewards = [0.0 for _ in range(self.config.multisteps)]
-        self.recent_done = [False for _ in range(self.config.multisteps)]
-        self.recent_invalid_actions = [[] for _ in range(self.config.multisteps + 1)]
+        # states : burnin + sequence_length + next_state
+        # actions: sequence_length
+        # probs  : sequence_length
+        # rewards: sequence_length
+        # done   : sequence_length
+        # invalid_actions: sequence_length + next_invalid_actions - now_invalid_actions
+        # hidden_state   : burnin + sequence_length + next_state
+
+        self.recent_states = [self.dummy_state for _ in range(self.config.burnin + self.config.sequence_length + 1)]
+        self.recent_actions = [
+            random.randint(0, self.config.nb_actions - 1) for _ in range(self.config.sequence_length)
+        ]
+        self.recent_probs = [1.0 / self.config.nb_actions for _ in range(self.config.sequence_length)]
+        self.recent_rewards = [0.0 for _ in range(self.config.sequence_length)]
+        self.recent_done = [False for _ in range(self.config.sequence_length)]
+        self.recent_invalid_actions = [[] for _ in range(self.config.sequence_length)]
 
         self.hidden_state = self.parameter.q_online.init_hidden_state()
         self.recent_hidden_states = [
             [self.hidden_state[0][0], self.hidden_state[1][0]]
-            for _ in range(self.config.burnin + self.config.multisteps + 1)
+            for _ in range(self.config.burnin + self.config.sequence_length + 1)
         ]
 
         self.recent_states.pop(0)
         self.recent_states.append(state.astype(np.float32))
         self.recent_invalid_actions.pop(0)
         self.recent_invalid_actions.append(invalid_actions)
+
+        # TD誤差を計算するか
+        if self.config.memory_name == "ReplayMemory":
+            self._calc_td_error = False
+        elif not self.distributed:
+            self._calc_td_error = False
+        else:
+            self._calc_td_error = True
+            self._history_batch = []
 
     def call_policy(self, state: np.ndarray, invalid_actions: List[int]) -> int:
         state = np.asarray([[state]] * self.config.batch_size)
@@ -458,7 +528,7 @@ class Worker(DiscreteActionWorker):
         self.action = random_choice_by_probs(probs)
 
         self.prob = probs[self.action]
-        self.q = q[self.action]
+        self.q = q
         return self.action
 
     def call_on_step(
@@ -491,7 +561,15 @@ class Worker(DiscreteActionWorker):
             ]
         )
 
-        priority = self._add_memory(self.q, None)
+        if self._calc_td_error:
+            calc_info = {
+                "q": self.q[self.action],
+                "reward": reward,
+            }
+        else:
+            calc_info = None
+
+        self._add_memory(calc_info)
 
         if done:
             # 残りstepも追加
@@ -510,12 +588,32 @@ class Worker(DiscreteActionWorker):
                 self.recent_invalid_actions.append([])
                 self.recent_hidden_states.pop(0)
 
-                self._add_memory(self.q, priority)
+                self._add_memory(
+                    {
+                        "q": self.q[self.action],
+                        "reward": 0.0,
+                    }
+                )
 
-        return {"priority": priority}
+                if self._calc_td_error:
+                    # TD誤差を計算してメモリに送る
+                    # targetQはモンテカルロ法
+                    reward = 0
+                    for batch, info in reversed(self._history_batch):
+                        if self.config.enable_rescale:
+                            _r = inverse_rescaling(reward)
+                        else:
+                            _r = reward
+                        reward = info["reward"] + self.config.gamma * _r
+                        if self.config.enable_rescale:
+                            reward = rescaling(reward)
 
-    def _add_memory(self, q, priority):
+                        td_error = reward - info["q"]
+                        self.remote_memory.add(batch, td_error)
 
+        return {}
+
+    def _add_memory(self, calc_info):
         batch = {
             "states": self.recent_states[self.config.burnin :],
             "actions": self.recent_actions[:],
@@ -527,27 +625,20 @@ class Worker(DiscreteActionWorker):
             "hidden_states": self.recent_hidden_states[0],
         }
 
-        # priority
-        if priority is None:
-            if self.config.memory_name == "ReplayMemory":
-                priority = 0
-            elif not self.distributed:
-                priority = 0
-            else:
-                priority = 0
-                # target_q = self.parameter.calc_target_q([batch])[0]
-                # priority = abs(target_q - q) + 0.0001
-
-        self.remote_memory.add(batch, priority)
-        return priority
+        if self._calc_td_error:
+            # エピソード最後に計算してメモリに送る
+            self._history_batch.append([batch, calc_info])
+        else:
+            # 計算する必要がない場合はそのままメモリに送る
+            self.remote_memory.add(batch, None)
 
     def call_render(self, env: EnvRun) -> None:
-        state = self.recent_states[-1]
         invalid_actions = self.recent_invalid_actions[-1]
-        state = np.asarray([[state]] * self.config.batch_size)
-        q, self.hidden_state = self.parameter.q_online(state, self.hidden_state)
-        q = q[0].numpy()
+        # パラメータを予測するとhidden_stateが変わってしまうの予測はしない
+        q = self.q
         maxa = np.argmax(q)
+        if self.config.enable_rescale:
+            q = inverse_rescaling(q)
 
         def _render_sub(a: int) -> str:
             return f"{q[a]:7.5f}"

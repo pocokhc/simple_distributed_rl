6c85e8c3fdc984e6d998cad22900bfe2327c8f84
poco_cpp <pocopococpp198@gmail.com>
2022-06-22 19:57:11 +0900

    displayを削除

--- tests/runner/test_render.py
+++ tests/runner/test_render.py
@@ -2,7 +2,6 @@ import unittest
 
 import srl
 from srl.runner import sequence
-from srl.utils import common as C
 
 
 class Test(unittest.TestCase):
@@ -11,26 +10,21 @@ class Test(unittest.TestCase):
         config = sequence.Config(srl.envs.Config("Grid"), None)
         _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
         render.create_anime().save("tmp/a.gif")
-        render.display()
 
         config = sequence.Config(srl.envs.Config("Grid"), None)
         _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
         render.create_anime().save("tmp/b.gif")
-        render.display()
 
     def test_gym(self):
 
         config = sequence.Config(srl.envs.Config("MountainCar-v0"), None)
         _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
         render.create_anime().save("tmp/c.gif")
-        render.display()
 
         config = sequence.Config(srl.envs.Config("MountainCar-v0"), None)
         _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
         render.create_anime().save("tmp/d.gif")
-        render.display()
 
 
 if __name__ == "__main__":
-    C.set_logger()
     unittest.main(module=__name__, defaultTest="Test.test_play", verbosity=2)

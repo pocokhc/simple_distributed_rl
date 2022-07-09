3e348ed75c7ce2c0821ab0cd793f4d507705ecbb
poco_cpp <pocopococpp198@gmail.com>
2022-07-09 18:10:24 +0900

    bug fix

--- tests/runner/test_render.py
+++ tests/runner/test_render.py
@@ -8,21 +8,21 @@ class Test(unittest.TestCase):
     def test_play(self):
 
         config = sequence.Config(srl.envs.Config("Grid"), None)
-        _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
+        _, render = sequence.render(config, max_steps=10, enable_animation=True)
         render.create_anime().save("tmp/a.gif")
 
         config = sequence.Config(srl.envs.Config("Grid"), None)
-        _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
+        _, render = sequence.render(config, max_steps=10, enable_animation=True)
         render.create_anime().save("tmp/b.gif")
 
     def test_gym(self):
 
         config = sequence.Config(srl.envs.Config("MountainCar-v0"), None)
-        _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
+        _, render = sequence.render(config, max_steps=10, enable_animation=True)
         render.create_anime().save("tmp/c.gif")
 
         config = sequence.Config(srl.envs.Config("MountainCar-v0"), None)
-        _, render = sequence.render(config, max_steps=10, mode="", enable_animation=True)
+        _, render = sequence.render(config, max_steps=10, enable_animation=True)
         render.create_anime().save("tmp/d.gif")
 
 

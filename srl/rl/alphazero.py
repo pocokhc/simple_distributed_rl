745650ff7ded251b614b1fd0ef4a9077c73a6f25
poco_cpp <pocopococpp198@gmail.com>
2022-07-17 11:38:52 +0900

    networkの引数微修正

--- srl/rl/alphazero.py
+++ srl/rl/alphazero.py
@@ -1,6 +1,5 @@
 import pickle
 import random
-from collections import deque
 from dataclasses import dataclass
 from typing import Any, List, cast
 
@@ -180,21 +179,11 @@ class _Network(keras.Model):
 
         # --- policy output
         c1 = config.policy_block(**config.policy_block_kwargs)(c1)
-        policy = kl.Dense(
-            config.action_num,
-            activation="softmax",
-            use_bias=False,
-            kernel_regularizer=regularizers.l2(_l2),
-        )(c1)
+        policy = kl.Dense(config.action_num, activation="softmax")(c1)
 
         # --- value output
         c2 = config.value_block(**config.value_block_kwargs)(c2)
-        value = kl.Dense(
-            1,
-            activation="tanh",
-            use_bias=False,
-            kernel_regularizer=regularizers.l2(_l2),
-        )(c2)
+        value = kl.Dense(1, activation="tanh")(c2)
 
         self.model = keras.Model(in_state, [policy, value], name="PVNetwork")
 

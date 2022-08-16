be22d7f98f7b785bb94e5fa522c1826881181ca5
poco_cpp <pocopococpp198@gmail.com>
2022-08-16 12:56:01 +0900

    リファクタリング

--- srl/rl/functions/common_tf.py
+++ srl/rl/functions/common_tf.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 # @tf.function
 def compute_logprob(mean, stddev, action):
     a1 = -0.5 * np.log(2 * np.pi)
-    stddev = tf.clip_by_value(stddev, 1e-6, np.inf)  # log(0)回避用
+    stddev = tf.clip_by_value(stddev, 1e-6, stddev)  # log(0)回避用
     a2 = -tf.math.log(stddev)
     a3 = -0.5 * tf.square((action - mean) / stddev)
     return a1 + a2 + a3
@@ -17,6 +17,6 @@ def compute_logprob(mean, stddev, action):
 def compute_logprob_sgp(mean, stddev, action):
     logmu = compute_logprob(mean, stddev, action)
     logpi = 1.0 - tf.tanh(action) ** 2
-    logpi = tf.clip_by_value(logpi, 1e-6, 1.0)  # log(0)回避用
+    logpi = tf.clip_by_value(logpi, 1e-6, logpi)  # log(0)回避用
     logpi = logmu - tf.math.log(logpi)
     return logpi

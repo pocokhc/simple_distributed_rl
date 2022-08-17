c8b323cc7dcccfe003f388b74db735b3b49f4c2b
poco_cpp <pocopococpp198@gmail.com>
2022-08-17 17:46:15 +0900

    RenderImageProcessorを追加

--- srl/base/rl/processors/__init__.py
+++ srl/base/rl/processors/__init__.py
@@ -1,1 +1,2 @@
 from .image_processor import ImageProcessor  # noqa F402
+from .render_image_processor import RenderImageProcessor  # noqa F402
---
+++ srl/base/rl/processors/render_image_processor.py
@@ -0,0 +1,51 @@
+import logging
+from dataclasses import dataclass
+from typing import Tuple
+
+from srl.base.define import EnvObservation, EnvObservationType, RLObservationType
+from srl.base.env.base import EnvRun, SpaceBase
+from srl.base.env.spaces.box import BoxSpace
+from srl.base.rl.processor import Processor
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class RenderImageProcessor(Processor):
+
+    render_kwargs: dict = None
+
+    def __post_init__(self):
+        if self.render_kwargs is None:
+            self.render_kwargs = {}
+
+    def change_observation_info(
+        self,
+        env_observation_space: SpaceBase,
+        env_observation_type: EnvObservationType,
+        rl_observation_type: RLObservationType,
+        env: EnvRun,
+    ) -> Tuple[SpaceBase, EnvObservationType]:
+
+        try:
+            env.reset()
+            rgb_array = env.render_rgb_array(**self.render_kwargs)
+            self.shape = rgb_array.shape
+            self.enable_rgb_array = True
+        except NotImplementedError:
+            self.enable_rgb_array = False
+
+        if not self.enable_rgb_array:
+            return env_observation_space, env_observation_type
+
+        new_space = BoxSpace(self.shape, 0, 255)
+        return new_space, EnvObservationType.COLOR
+
+    def process_observation(
+        self,
+        observation: EnvObservation,
+        env: EnvRun,
+    ) -> EnvObservation:
+        if not self.enable_rgb_array:
+            return observation
+        return env.render_rgb_array(**self.render_kwargs)

f60d6bc045435dea7b11ca1c3563142e27f2725e
poco_cpp <pocopococpp198@gmail.com>
2022-09-23 15:37:42 +0900

    humanの挙動を改善

--- srl/rl/human.py
+++ srl/rl/human.py
@@ -3,6 +3,8 @@ from typing import Tuple
 
 from srl.base.define import EnvAction
 from srl.base.env.base import EnvRun
+from srl.base.env.spaces.continuous import ContinuousSpace
+from srl.base.env.spaces.discrete import DiscreteSpace
 from srl.base.rl.registration import register_worker
 from srl.base.rl.worker import RuleBaseWorker, WorkerRun
 
@@ -17,18 +19,33 @@ class Worker(RuleBaseWorker):
         return {}
 
     def call_policy(self, env: EnvRun, worker: WorkerRun) -> Tuple[EnvAction, dict]:
-        valid_actions = env.get_valid_actions()
+        if isinstance(env.action_space, DiscreteSpace):
+            valid_actions = env.get_valid_actions()
 
-        print(f"select action: {valid_actions}")
-        for i in range(10):
-            try:
-                action = int(input("> "))
-                if action in valid_actions:
-                    break
-            except Exception:
+            print("- select action -")
+            for action in valid_actions:
+                a1 = str(action)
+                a2 = env.action_to_str(action)
+                if a1 == a2:
+                    print(f"{a1:>3s}")
+                else:
+                    print(f"{a1:>3s}: {a2}")
+            for i in range(10):
+                try:
+                    action = int(input("> "))
+                    if action in valid_actions:
+                        break
+                except Exception:
+                    pass
                 print(f"invalid action({10-i} times left)")
-        else:
-            raise ValueError()
+            else:
+                raise ValueError()
 
-        action = env.action_space.action_discrete_decode(action)
-        return action, {}
+        elif isinstance(env.action_space, ContinuousSpace):
+            print(f"{env.action_space.low} - {env.action_space.high} >", end="")
+            action = float(input())
+
+        else:
+            assert False, "Cannot input from terminal."
+
+        return env.action_space.convert(action), {}

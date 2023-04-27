39f8cf9842d1f9c76946da3b4d6e1a20e63d5c28
poco_cpp <pocopococpp198@gmail.com>
2023-04-27 19:08:39 +0900

    挙動が怪しかったので書き換え

--- tests/other/runner/test_config.py
+++ tests/other/runner/test_config.py
@@ -5,10 +5,11 @@ from srl.utils import common
 
 common.logger_print()
 
+# pytest.mark.parametrize に記号が入ると動作が変？
 
-@pytest.mark.parametrize(
-    "run_name, device, distributed, true_device",
-    [
+
+def test_get_device_name_main():
+    for run_name, device, distributed, true_device in [
         ("main", "", False, "AUTO"),
         ("main", "", True, "CPU"),
         ("main", "AUTO", False, "AUTO"),
@@ -21,49 +22,42 @@ common.logger_print()
         ("eval", "AUTO", True, "CPU"),
         ("eval", "CPU:0", False, "CPU:0"),
         ("eval", "GPU:0", False, "GPU:0"),
-    ],
-)
-def test_get_device_name_main(run_name, device, distributed, true_device):
-    config = runner.Config("Grid", None)
-    config.run_name = run_name
-    config.device_main = device
-    config.distributed = distributed
-    assert config.get_device_name() == true_device
+    ]:
+        config = runner.Config("Grid", None)
+        config.run_name = run_name
+        config.device_main = device
+        config.distributed = distributed
+        assert config.get_device_name() == true_device
 
 
-@pytest.mark.parametrize(
-    "device, true_device",
-    [
+def test_get_device_name_trainer():
+    for device, true_device in [
         ("", "AUTO"),
         ("AUTO", "AUTO"),
         ("CPU:0", "CPU:0"),
         ("GPU:0", "GPU:0"),
-    ],
-)
-def test_get_device_name_trainer(device, true_device):
-    config = runner.Config("Grid", None)
-    config.run_name = "trainer"
-    config.device_mp_trainer = device
-    assert config.get_device_name() == true_device
+    ]:
+        config = runner.Config("Grid", None)
+        config.run_name = "trainer"
+        config.device_mp_trainer = device
+        assert config.get_device_name() == true_device
 
 
-@pytest.mark.parametrize(
-    "device, actor_id, true_device",
-    [
+def test_get_device_name_actor():
+    for device, actor_id, true_device in [
         ("", 0, "CPU"),
         ("AUTO", 0, "CPU"),
         ("GPU:0", 0, "GPU:0"),
         ("CPU:0", 0, "CPU:0"),
         ("CPU:0", 1, "CPU:0"),
         (["CPU:0", "CPU:1"], 1, "CPU:1"),
-    ],
-)
-def test_get_device_name_actor(device, actor_id, true_device):
-    config = runner.Config("Grid", None)
-    config.run_name = f"actor{actor_id}"
-    config.run_actor_id = actor_id
-    config.device_mp_actors = device
-    assert config.get_device_name() == true_device
+
+    ]:
+        config = runner.Config("Grid", None)
+        config.run_name = f"actor{actor_id}"
+        config.run_actor_id = actor_id
+        config.device_mp_actors = device
+        assert config.get_device_name() == true_device
 
 
 def _main_init_device(device, true_device, module):
@@ -90,45 +84,37 @@ def _main_init_device(device, true_device, module):
         assert config.used_device_torch == true_device, f"{config.used_device_torch} == {true_device} is fail."
 
 
-@pytest.mark.parametrize(
-    "device, true_device, module",
-    [
+def test_main_cpu_init_device():
+    for device, true_device, module in [
         ["cPU", "/CPU", "tensorflow"],
         ["CPU:1", "/CPU:1", "tensorflow"],
         ["CPU", "cpu", "torch"],
         ["CPU:1", "cpu:1", "torch"],
-    ],
-)
-def test_main_cpu_init_device(device, true_device, module):
-    _main_init_device(device, true_device, module)
+    ]:
+        _main_init_device(device, true_device, module)
 
 
-@pytest.mark.parametrize(
-    "device, true_device",
-    [
-        ["gPU", "/GPU"],
-        ["GPU:1", "/GPU:1"],
-    ],
-)
-def test_main_tf_gpu_init_device(device, true_device):
+def test_main_tf_gpu_init_device():
     pytest.importorskip("tensorflow")
     if not common.is_available_gpu_tf():
         pytest.skip()
-    _main_init_device(device, true_device, "tensorflow")
+
+    for device, true_device, module in [
+        ["gPU", "/GPU", "tensorflow"],
+        ["GPU:1", "/GPU:1", "tensorflow"],
+    ]:
+        _main_init_device(device, true_device, module)
 
 
-@pytest.mark.parametrize(
-    "device, true_device, module",
-    [
-        ["gPU", "cuda", "torch"],
-        ["GPU:1", "cuda:1", "torch"],
-    ],
-)
-def test_main_torch_gpu_init_device(device, true_device, module):
+def test_main_torch_gpu_init_device():
     pytest.importorskip("torch")
     if not common.is_available_gpu_torch():
         pytest.skip()
-    _main_init_device(device, true_device, "torch")
+    for device, true_device in [
+        ["gPU", "cuda"],
+        ["GPU:1", "cuda:1"],
+    ]:
+        _main_init_device(device, true_device, "torch")
 
 
 def test_main_tf_auto_cpu_init_device():

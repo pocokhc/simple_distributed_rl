cdf3978e3c3c9cb34c4ccd77e371a1f64563907d
poco_cpp <pocopococpp198@gmail.com>
2023-05-20 14:19:21 +0900

    remoteの更新忘れ

--- srl/runner/__init__.py
+++ srl/runner/__init__.py
@@ -5,9 +5,8 @@ from .core import HistoryOption  # noqa F401
 from .core import ProgressOption  # noqa F401
 from .mp import train_mp  # noqa F401
 from .play_game import play  # noqa F401
-
-# from .remote import run_actor  # noqa F401
-# from .remote import train_remote  # noqa F401
+from .remote import run_actor  # noqa F401
+from .remote import train_remote  # noqa F401
 from .sequence import animation  # noqa F401
 from .sequence import evaluate  # noqa F401
 from .sequence import render  # noqa F401

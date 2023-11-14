2dd41348f6be14cda1be7e85940bba02114cde43
poco_cpp <pocopococpp198@gmail.com>
2023-11-14 22:01:00 +0900

    [examples] update: sample atari更新

--- examples/sample_atari.py
+++ examples/sample_atari.py
@@ -9,11 +9,12 @@ from srl.utils import common
 common.set_logger()
 
 _parameter_path = "_sample_atari_parameter.dat"
+_history_path = "_sample_atari_history"
 
 TRAIN_COUNT = 500_000
 
 
-def _create_runner(load_parameter: bool):
+def _create_runner():
     # --- Atari env
     # Run "pip install gymnasium pygame" and also see the URL below.
     # https://gymnasium.farama.org/environments/atari/
@@ -36,6 +37,9 @@ def _create_runner(load_parameter: bool):
     rl_config.memory.set_proportional_memory(beta_steps=TRAIN_COUNT)
     rl_config.image_block.set_r2d3_image()
     rl_config.hidden_block.set_mlp((512,))
+    rl_config.window_length = 4
+
+    # カスタムしたprocessorを追加
     rl_config.processors = [
         ImageProcessor(
             image_type=EnvObservationTypes.GRAY_2ch,
@@ -44,30 +48,32 @@ def _create_runner(load_parameter: bool):
             enable_norm=True,
         )
     ]
-    rl_config.use_rl_processor = False
-    rl_config.window_length = 4
+    rl_config.use_rl_processor = False  # アルゴリズムデフォルトのprocessorを無効にする
+
     runner = srl.Runner(env_config, rl_config)
-
-    # --- load parameter
-    if load_parameter:
-        runner.load_parameter(_parameter_path)
-
     return runner
 
 
 def train():
-    runner = _create_runner(load_parameter=False)
+    runner = _create_runner()
 
     # (option) print tensorflow model
     runner.model_summary(expand_nested=True)
 
     # --- train
+    runner.set_history_on_file(_history_path, interval=5)
     runner.train_mp(actor_num=1, max_train_count=TRAIN_COUNT)
     runner.save_parameter(_parameter_path)
 
 
+def plot_history():
+    history = srl.Runner.load_history(_history_path)
+    history.plot()
+
+
 def evaluate():
-    runner = _create_runner(load_parameter=True)
+    runner = _create_runner()
+    runner.load_parameter(_parameter_path)
 
     # --- evaluate
     rewards = runner.evaluate(max_episodes=10)
@@ -79,4 +85,5 @@ def evaluate():
 
 if __name__ == "__main__":
     train()
+    plot_history()
     evaluate()

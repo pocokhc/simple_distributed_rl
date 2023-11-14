a3620352deca42b37ec82a5467ce22d83d24c940
poco_cpp <pocopococpp198@gmail.com>
2023-11-14 19:42:27 +0900

    [runner] new: wkdirによる連続学習環境のsample追加

---
+++ examples/sample_long_training.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+import srl
+from srl.algorithms import ql  # algorithm load
+
+
+def _create_runner():
+    # (Run "pip install gymnasium pygame" to use the gymnasium environment)
+    env_config = srl.EnvConfig("FrozenLake-v1")
+    rl_config = ql.Config()
+    runner = srl.Runner(env_config, rl_config)
+
+    # --- Set directory to save learning
+    # Parameters will be saved automatically.
+    # The previous parameters will be automatically loaded.
+    runner.setup_wkdir("_sample_long_training")
+
+    return runner
+
+
+def train():
+    runner = _create_runner()
+    runner.train(timeout=10)
+
+
+def evaluate():
+    runner = _create_runner()
+    rewards = runner.evaluate(max_episodes=100)
+    print(f"Average reward for 100 episodes: {np.mean(rewards, axis=0)}")
+
+
+if __name__ == "__main__":
+    train()
+    evaluate()

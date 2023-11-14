c1d2b5bdfd6137b4b90f2371699da12babd48eaf
poco_cpp <pocopococpp198@gmail.com>
2023-11-14 22:04:59 +0900

    make html

---
+++ docs/_sources/pages/algorithms/agent57.rst.txt
@@ -0,0 +1,5 @@
+
+Agent57
+===============
+.. autoclass:: srl.algorithms.agent57.Config
+   :members:
--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.12.2.3',
+    VERSION: '0.13.1',
     LANGUAGE: 'ja',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

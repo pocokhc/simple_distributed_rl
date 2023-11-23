bc2cdd1245e4a2b96c5287dfbed71c73dd4b20cb
poco_cpp <pocopococpp198@gmail.com>
2023-11-23 11:24:49 +0900

    [runner.mp_debug] delete: mp_debugを一時的に削除

--- srl/runner/runner_facade.py
+++ srl/runner/runner_facade.py
@@ -391,114 +391,114 @@ class RunnerFacade(Runner):
 
         self._base_run_play_after()
 
-    def train_mp_debug(
-        self,
-        # mp
-        actor_num: int = 1,
-        queue_capacity: int = 1000,
-        trainer_parameter_send_interval: int = 1,
-        actor_parameter_sync_interval: int = 1,
-        enable_prepare_sample_batch: bool = False,
-        device_actors: Union[str, List[str]] = "AUTO",
-        # --- stop config
-        max_episodes: int = -1,
-        timeout: float = -1,
-        max_steps: int = -1,
-        max_train_count: int = -1,
-        max_memory: int = -1,
-        # --- play config
-        shuffle_player: bool = True,
-        # --- progress
-        enable_progress: bool = True,
-        progress_start_time: int = 1,
-        progress_interval_limit: int = 60 * 10,
-        progress_env_info: bool = False,
-        progress_train_info: bool = True,
-        progress_worker_info: bool = True,
-        progress_worker: int = 0,
-        progress_max_actor: int = 5,
-        # --- eval
-        enable_eval: bool = False,
-        eval_env_sharing: bool = False,
-        eval_episode: int = 1,
-        eval_timeout: float = -1,
-        eval_max_steps: int = -1,
-        eval_players: List[Union[None, StrWorkerType, RLWorkerType]] = [],
-        eval_shuffle_player: bool = False,
-        # --- other
-        callbacks: List[CallbackType] = [],
-        # --- debug option
-        choice_method: str = "random",
-    ):
-        """multiprocessingの分散学習と出来る限り似た学習を、single processで実施します。
-        ほとんどの引数は train_mp と同じなのです。
+    # def train_mp_debug(
+    #     self,
+    #     # mp
+    #     actor_num: int = 1,
+    #     queue_capacity: int = 1000,
+    #     trainer_parameter_send_interval: int = 1,
+    #     actor_parameter_sync_interval: int = 1,
+    #     enable_prepare_sample_batch: bool = False,
+    #     device_actors: Union[str, List[str]] = "AUTO",
+    #     # --- stop config
+    #     max_episodes: int = -1,
+    #     timeout: float = -1,
+    #     max_steps: int = -1,
+    #     max_train_count: int = -1,
+    #     max_memory: int = -1,
+    #     # --- play config
+    #     shuffle_player: bool = True,
+    #     # --- progress
+    #     enable_progress: bool = True,
+    #     progress_start_time: int = 1,
+    #     progress_interval_limit: int = 60 * 10,
+    #     progress_env_info: bool = False,
+    #     progress_train_info: bool = True,
+    #     progress_worker_info: bool = True,
+    #     progress_worker: int = 0,
+    #     progress_max_actor: int = 5,
+    #     # --- eval
+    #     enable_eval: bool = False,
+    #     eval_env_sharing: bool = False,
+    #     eval_episode: int = 1,
+    #     eval_timeout: float = -1,
+    #     eval_max_steps: int = -1,
+    #     eval_players: List[Union[None, StrWorkerType, RLWorkerType]] = [],
+    #     eval_shuffle_player: bool = False,
+    #     # --- other
+    #     callbacks: List[CallbackType] = [],
+    #     # --- debug option
+    #     choice_method: str = "random",
+    # ):
+    #     """multiprocessingの分散学習と出来る限り似た学習を、single processで実施します。
+    #     ほとんどの引数は train_mp と同じなのです。
 
-        Args:
-            choice_method(str, optional): 各actorとtrainerの採用方法を指定します. Defaults to 'random'.
-        """
-        callbacks = callbacks[:]
+    #     Args:
+    #         choice_method(str, optional): 各actorとtrainerの採用方法を指定します. Defaults to 'random'.
+    #     """
+    #     callbacks = callbacks[:]
 
-        self.context.actor_num = actor_num
-        self.config.dist_queue_capacity = queue_capacity
-        self.config.trainer_parameter_send_interval = trainer_parameter_send_interval
-        self.config.actor_parameter_sync_interval = actor_parameter_sync_interval
-        self.config.dist_enable_prepare_sample_batch = enable_prepare_sample_batch
-        self.config.device_actors = device_actors
+    #     self.context.actor_num = actor_num
+    #     self.config.dist_queue_capacity = queue_capacity
+    #     self.config.trainer_parameter_send_interval = trainer_parameter_send_interval
+    #     self.config.actor_parameter_sync_interval = actor_parameter_sync_interval
+    #     self.config.dist_enable_prepare_sample_batch = enable_prepare_sample_batch
+    #     self.config.device_actors = device_actors
 
-        # --- set context
-        self.context.run_name = RunNameTypes.main
-        # stop config
-        self.context.max_episodes = max_episodes
-        self.context.timeout = timeout
-        self.context.max_steps = max_steps
-        self.context.max_train_count = max_train_count
-        self.context.max_memory = max_memory
-        # play config
-        self.context.shuffle_player = shuffle_player
-        self.context.disable_trainer = False
-        # play info
-        self.context.distributed = True
-        self.context.training = True
-        self.context.render_mode = RenderModes.none
+    #     # --- set context
+    #     self.context.run_name = RunNameTypes.main
+    #     # stop config
+    #     self.context.max_episodes = max_episodes
+    #     self.context.timeout = timeout
+    #     self.context.max_steps = max_steps
+    #     self.context.max_train_count = max_train_count
+    #     self.context.max_memory = max_memory
+    #     # play config
+    #     self.context.shuffle_player = shuffle_player
+    #     self.context.disable_trainer = False
+    #     # play info
+    #     self.context.distributed = True
+    #     self.context.training = True
+    #     self.context.render_mode = RenderModes.none
 
-        # --- progress ---
-        if enable_progress:
-            from srl.runner.callbacks.print_progress import PrintProgress
+    #     # --- progress ---
+    #     if enable_progress:
+    #         from srl.runner.callbacks.print_progress import PrintProgress
 
-            callbacks.append(
-                PrintProgress(
-                    start_time=progress_start_time,
-                    interval_limit=progress_interval_limit,
-                    progress_env_info=progress_env_info,
-                    progress_train_info=progress_train_info,
-                    progress_worker_info=progress_worker_info,
-                    progress_worker=progress_worker,
-                    progress_max_actor=progress_max_actor,
-                    enable_eval=enable_eval,
-                    eval_env_sharing=eval_env_sharing,
-                    eval_episode=eval_episode,
-                    eval_timeout=eval_timeout,
-                    eval_max_steps=eval_max_steps,
-                    eval_players=eval_players,
-                    eval_shuffle_player=eval_shuffle_player,
-                )
-            )
-            logger.info("add callback PrintProgress")
-        # ----------------
+    #         callbacks.append(
+    #             PrintProgress(
+    #                 start_time=progress_start_time,
+    #                 interval_limit=progress_interval_limit,
+    #                 progress_env_info=progress_env_info,
+    #                 progress_train_info=progress_train_info,
+    #                 progress_worker_info=progress_worker_info,
+    #                 progress_worker=progress_worker,
+    #                 progress_max_actor=progress_max_actor,
+    #                 enable_eval=enable_eval,
+    #                 eval_env_sharing=eval_env_sharing,
+    #                 eval_episode=eval_episode,
+    #                 eval_timeout=eval_timeout,
+    #                 eval_max_steps=eval_max_steps,
+    #                 eval_players=eval_players,
+    #                 eval_shuffle_player=eval_shuffle_player,
+    #             )
+    #         )
+    #         logger.info("add callback PrintProgress")
+    #     # ----------------
 
-        self._base_run_play_before(
-            enable_checkpoint_load=True,
-            enable_checkpoint=True,
-            enable_history_on_memory=False,
-            enable_history_on_file=True,
-            callbacks=callbacks,
-        )
+    #     self._base_run_play_before(
+    #         enable_checkpoint_load=True,
+    #         enable_checkpoint=True,
+    #         enable_history_on_memory=False,
+    #         enable_history_on_file=True,
+    #         callbacks=callbacks,
+    #     )
 
-        from .core_mp_debug import train
+    #     from .core_mp_debug import train
 
-        train(self, choice_method)
+    #     train(self, choice_method)
 
-        self._base_run_play_after()
+    #     self._base_run_play_after()
 
     def train_distribution(
         self,

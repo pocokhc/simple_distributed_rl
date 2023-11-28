fb642c0e92b44e3a374065daed8928f133da8c48
poco_cpp <pocopococpp198@gmail.com>
2023-11-28 20:13:45 +0900

    [rl.processors] new: AtariProcessor追加

---
+++ srl/rl/processors/atari_processor.py
@@ -0,0 +1,26 @@
+from srl.base.env.env_run import EnvRun
+from srl.base.rl.config import RLConfig
+from srl.base.rl.processor import Processor
+from srl.utils.common import is_package_installed
+
+
+class AtariProcessor(Processor):
+    def __init__(self):
+        assert is_package_installed("ale_py")
+
+    def setup(self, env: EnvRun, rl_config: RLConfig):
+        self.enable = "AtariEnv" in str(env.get_original_env())
+
+    def on_reset(self, env: EnvRun):
+        if not self.enable:
+            return
+        self.lives = env.get_original_env().env.unwrapped.ale.lives()
+
+    def preprocess_done(self, done: bool, env: EnvRun) -> bool:
+        if not self.enable:
+            return done
+        new_lives = env.get_original_env().env.unwrapped.ale.lives()
+        if new_lives < self.lives:
+            return True
+        self.lives = new_lives
+        return done

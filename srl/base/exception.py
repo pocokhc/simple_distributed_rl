aa3a91a38e9fc9725775c3aa341b14c0575c1318
poco_cpp <pocopococpp198@gmail.com>
2023-12-17 21:11:48 +0900

    [base.exception] new: Exceptionを追加

--- srl/base/exception.py
+++ srl/base/exception.py
@@ -3,5 +3,13 @@ class SRLError(Exception):
         return "%s: An unspecified SRL error has occurred; %s" % (self.__class__.__name__, self.args)
 
 
+class TFLayerError(SRLError):
+    pass
+
+
+class UndefinedError(SRLError):
+    pass
+
+
 class DistributionError(SRLError):
     pass

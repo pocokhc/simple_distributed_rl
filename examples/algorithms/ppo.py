7bcb17b0e7e8405db6ab5e363c64507ff77887cb
poco_cpp <pocopococpp198@gmail.com>
2023-12-24 21:54:34 +0900

    [algorithms] update: PPO

--- examples/algorithms/ppo.py
+++ examples/algorithms/ppo.py
@@ -1,41 +1,48 @@
 import numpy as np
-
 import srl
 from srl.algorithms import ppo
+from srl.base.define import RLTypes
 from srl.utils import common
 
 common.logger_print()
 
 
-def main():
-    env_config = srl.EnvConfig("Grid")
+def main(mode: str):
+    env_config = srl.EnvConfig("EasyGrid")
     rl_config = ppo.Config(
-        batch_size=128,
-        discount=0.9,
-        gae_discount=0.9,
-        enable_value_clip=True,
-        enable_state_normalized=False,
-        baseline_type="normal",
+        batch_size=32,
+        baseline_type="",
         experience_collection_method="GAE",
         surrogate_type="clip",  # kl or clip
     )
-    rl_config.lr.set_linear(100_000, 0.01, 0.001)
+    rl_config.lr.set_constant(0.005)
     rl_config.memory.capacity = 1000
     rl_config.memory.warmup_size = 1000
     rl_config.hidden_block.set_mlp((64, 64))
-    rl_config.value_block.set_mlp((64,))
+    rl_config.value_block.set_mlp(())
     rl_config.policy_block.set_mlp(())
 
+    if mode == "CONTINUOUS":
+        TRAIN_COUNT = 40000
+        rl_config.lr.set_constant(0.001)
+        rl_config.entropy_weight = 1.0
+        rl_config.override_action_type = RLTypes.CONTINUOUS
+    else:
+        TRAIN_COUNT = 20000
+
     runner = srl.Runner(env_config, rl_config)
-    runner.model_summary(expand_nested=True)
+    runner.model_summary()
 
     # --- train
-    runner.train(max_train_count=30_000)
+    runner.train(max_train_count=TRAIN_COUNT)
 
     # --- evaluate
     rewards = runner.evaluate(max_episodes=100)
     print(f"Average reward for 100 episodes: {np.mean(rewards)}")
 
+    runner.replay_window()
+
 
 if __name__ == "__main__":
-    main()
+    main("DISCRETE")
+    main("CONTINUOUS")
--- tests/algorithms_/base_ppo.py
+++ tests/algorithms_/base_ppo.py
@@ -1,5 +1,3 @@
-import pytest
-
 from srl.base.define import RLTypes
 
 from .common_base_class import CommonBaseClass
@@ -10,62 +8,82 @@ class BaseCase(CommonBaseClass):
         from srl.algorithms import ppo
 
         rl_config = ppo.Config(
-            batch_size=128,
+            batch_size=32,
             discount=0.9,
+            gae_discount=0.9,
             surrogate_type="clip",
             baseline_type="normal",
             experience_collection_method="MC",
             enable_value_clip=False,
             enable_state_normalized=False,
         )
-        rl_config.lr.set_linear(1000, 0.01, 0.001)
-        rl_config.hidden_block.set_mlp((32, 32))
+        rl_config.lr.set_constant(0.005)
+        rl_config.hidden_block.set_mlp((64, 64))
+        rl_config.value_block.set_mlp(())
+        rl_config.policy_block.set_mlp(())
         rl_config.memory.capacity = 1000
         rl_config.memory.warmup_size = 1000
         return rl_config
 
-    def test_Grid(self):
-        self.check_skip()
-        rl_config = self._create_rl_config()
-        runner, tester = self.create_runner("Grid", rl_config)
-        runner.train(max_train_count=10000)
-        tester.eval(runner)
-
-    @pytest.mark.parametrize("baseline_type", ["", "ave", "std", "advantage"])
-    def test_Grid_baseline(self, baseline_type):
-        self.check_skip()
-        rl_config = self._create_rl_config()
-        rl_config.baseline_type = baseline_type
-        runner, tester = self.create_runner("Grid", rl_config)
-        runner.train(max_train_count=10000)
-        tester.eval(runner)
-
-    def test_Grid_GAE(self):
+    def test_EasyGrid1(self):
         self.check_skip()
         rl_config = self._create_rl_config()
         rl_config.experience_collection_method = "GAE"
+        rl_config.baseline_type = ""
+        rl_config.surrogate_type = "clip"
+        rl_config.enable_value_clip = True
+        rl_config.enable_state_normalized = False
+        runner, tester = self.create_runner("EasyGrid", rl_config)
+        runner.train(max_train_count=10000)
+        tester.eval(runner)
+
+    def test_Grid2(self):
+        self.check_skip()
+        rl_config = self._create_rl_config()
+        rl_config.experience_collection_method = "GAE"
+        rl_config.baseline_type = "v"
+        rl_config.surrogate_type = "clip"
+        rl_config.enable_value_clip = False
+        rl_config.enable_state_normalized = False
         runner, tester = self.create_runner("Grid", rl_config)
         runner.train(max_train_count=10000)
         tester.eval(runner)
 
-    def test_Grid_KL(self):
+    def test_Grid3(self):
         self.check_skip()
         rl_config = self._create_rl_config()
-        rl_config.surrogate_type = "KL"
+        rl_config.experience_collection_method = "MC"
+        rl_config.baseline_type = "normal"
+        rl_config.surrogate_type = "kl"
+        rl_config.enable_value_clip = False
+        rl_config.enable_state_normalized = False
         runner, tester = self.create_runner("Grid", rl_config)
-        runner.train(max_train_count=10000)
+        runner.train(max_train_count=30000)
         tester.eval(runner)
 
-    def test_Pendulum(self):
+    def test_Grid4(self):
         self.check_skip()
-        # うまく学習できない... TODO
         rl_config = self._create_rl_config()
+        rl_config.experience_collection_method = "MC"
+        rl_config.baseline_type = "normal"
+        rl_config.surrogate_type = ""  # ""は学習がそもそも難しい
+        rl_config.enable_value_clip = True
+        rl_config.enable_state_normalized = True
+        runner, tester = self.create_runner("Grid", rl_config)
+        runner.train(max_train_count=20000)
+        tester.eval(runner, baseline=-1)
+
+    def test_EasyGrid_continue(self):
+        self.check_skip()
+        rl_config = self._create_rl_config()
+        rl_config.lr.set_constant(0.001)
+        rl_config.experience_collection_method = "GAE"
+        rl_config.baseline_type = ""
+        rl_config.surrogate_type = "clip"
+        rl_config.enable_value_clip = False
+        rl_config.enable_state_normalized = False
+        rl_config.entropy_weight = 1.0
         rl_config.override_action_type = RLTypes.CONTINUOUS
-        rl_config.lr.set_constant(0.0001)
-        rl_config.value_loss_weight = 0.5
-        rl_config.entropy_weight = 0.0001
-        rl_config.hidden_block.set_mlp((128,))
-        rl_config.policy_block.set_mlp((128, 128))
-        runner, tester = self.create_runner("Pendulum-v1", rl_config)
-        runner.train(max_train_count=200 * 500)
+        runner, tester = self.create_runner("EasyGrid", rl_config)
+        runner.train(max_train_count=40000)
         tester.eval(runner)

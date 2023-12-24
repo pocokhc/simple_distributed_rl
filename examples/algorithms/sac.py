5239b5423578bf424be23258ca7798f2dbd84876
poco_cpp <pocopococpp198@gmail.com>
2023-12-24 21:55:02 +0900

    [algorithms] update: SAC

--- examples/algorithms/sac.py
+++ examples/algorithms/sac.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 import srl
 from srl.algorithms import sac
 from srl.utils import common
@@ -7,25 +6,57 @@ from srl.utils import common
 common.logger_print()
 
 
-def main():
-    env_config = srl.EnvConfig("Pendulum-v1")
-    rl_config = sac.Config(
-        lr_policy=0.003,
-        lr_q=0.003,
-        lr_alpha=0.001,
-    )
-    rl_config.policy_hidden_block.set_mlp((64, 64, 64))
-    rl_config.q_hidden_block.set_mlp((128, 128, 128))
+def main_discrete():
+    env_config = srl.EnvConfig("EasyGrid")
+    rl_config = sac.Config()
+    rl_config.batch_size = 32
+    rl_config.lr_policy.set_constant(0.0002)
+    rl_config.lr_q.set_constant(0.001)
+    rl_config.memory.capacity = 10000
+    rl_config.memory.warmup_size = 1000
+    rl_config.policy_hidden_block.set_mlp((32, 32, 32))
+    rl_config.q_hidden_block.set_mlp((32, 32, 32))
+    rl_config.entropy_bonus_exclude_q = True
+    rl_config.entropy_alpha = 0.1
+    rl_config.entropy_alpha_auto_scale = False
+
     runner = srl.Runner(env_config, rl_config)
-    runner.model_summary(expand_nested=True)
+    runner.model_summary()
+    runner.set_progress_options(interval_limit=30)
 
     # --- train
-    runner.train(max_episodes=25)
+    runner.train(max_train_count=5000)
 
     # --- evaluate
     rewards = runner.evaluate()
     print(f"Average reward: {np.mean(rewards)}")
 
+    runner.replay_window()
+
+
+def main_continuous():
+    env_config = srl.EnvConfig("Pendulum-v1")
+    rl_config = sac.Config()
+    rl_config.batch_size = 32
+    rl_config.lr_policy.set_constant(0.003)
+    rl_config.lr_q.set_constant(0.003)
+    rl_config.memory.capacity = 10000
+    rl_config.memory.warmup_size = 1000
+    rl_config.policy_hidden_block.set_mlp((64, 64, 64))
+    rl_config.q_hidden_block.set_mlp((128, 128, 128))
+    runner = srl.Runner(env_config, rl_config)
+    runner.model_summary()
+
+    # --- train
+    runner.train(max_episodes=30)
+
+    # --- evaluate
+    rewards = runner.evaluate()
+    print(f"Average reward: {np.mean(rewards)}")
+
+    runner.replay_window()
+
 
 if __name__ == "__main__":
-    main()
+    main_discrete()
+    main_continuous()

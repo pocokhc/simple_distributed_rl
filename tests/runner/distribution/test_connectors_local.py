e0017199d3e63eb987836b673313dfd441c477c8
poco_cpp <pocopococpp198@gmail.com>
2024-02-21 22:02:37 +0900

    [tests.runner.distribution] fix: redisがinstallされていない場合でpytestが動くように修正

--- tests/runner/distribution/test_connectors_local.py
+++ tests/runner/distribution/test_connectors_local.py
@@ -1,13 +1,16 @@
 import pytest
 
-from srl.runner.distribution.connectors.parameters import MQTTParameters, RabbitMQParameters, RedisParameters
 from srl.utils import common
-from tests.runner.distribution.memory_test_functions import memory_connector_error_test, memory_connector_test
+try:
+    from tests.runner.distribution.memory_test_functions import memory_connector_error_test, memory_connector_test
+except ModuleNotFoundError as e:
+    print(e)
 
 
 def test_redis():
     pytest.importorskip("redis")
     common.logger_print()
+    from srl.runner.distribution.connectors.parameters import RedisParameters
 
     memory_connector_test(RedisParameters(host="localhost"))
     memory_connector_error_test(RedisParameters(host="test"))
@@ -16,6 +19,7 @@ def test_redis():
 def test_rabbitmq():
     pytest.importorskip("pika")
     common.logger_print()
+    from srl.runner.distribution.connectors.parameters import RabbitMQParameters
 
     memory_connector_test(RabbitMQParameters(host="localhost", ssl=False))
     memory_connector_error_test(RabbitMQParameters(host="test", ssl=False))
@@ -23,7 +27,8 @@ def test_rabbitmq():
 
 def test_mqtt():
     pytest.importorskip("paho")
-
     common.logger_print()
+    from srl.runner.distribution.connectors.parameters import MQTTParameters
+
     memory_connector_test(MQTTParameters(host="localhost"))
     memory_connector_error_test(MQTTParameters(host="test"))

4b6b9278a5fce85e024a2a2769666ee27d724c5d
poco_cpp <pocopococpp198@gmail.com>
2024-03-16 13:09:26 +0900

    [rl.models] change: inputs_blockをMultiSpaceに対応

--- srl/rl/models/tf/input_block.py
+++ srl/rl/models/tf/input_block.py
@@ -1,10 +1,12 @@
+import enum
 import logging
+from typing import List, Optional, Tuple
 
 import tensorflow as tf
 from tensorflow import keras
 
 from srl.base.define import EnvTypes, RLTypes
-from srl.base.exception import TFLayerError, UndefinedError
+from srl.base.exception import NotSupportedError, TFLayerError, UndefinedError
 from srl.base.spaces.multi import MultiSpace
 from srl.base.spaces.space import SpaceBase
 from srl.rl.models.image_block import ImageBlockConfig
@@ -17,96 +19,214 @@ kl = keras.layers
 logger = logging.getLogger(__name__)
 
 
-class InputBlock(KerasModelAddedSummary):
-    def __init__(
-        self,
-        value_block_config: MLPBlockConfig,
-        image_block_config: ImageBlockConfig,
-        observation_space: SpaceBase,
-        enable_time_distributed_layer: bool = False,
-    ):
-        super().__init__()
+class MultiVariableTypes(enum.Enum):
+    VALUE = enum.auto()
+    IMAGE = enum.auto()
 
-        if isinstance(observation_space, MultiSpace):
-            self.is_multi_input = True
-            self.in_multi_layers = []
-            self.in_multi_size = len(observation_space.spaces)
-            for space in observation_space.spaces:
-                if space.rl_type == RLTypes.IMAGE:
-                    layers = self._create_input_image_layers(space)
-                    layers.append(image_block_config.create_block_tf(enable_time_distributed_layer))
-                    layers.append(kl.Flatten())
-                    self.in_multi_layers.append(layers)
-                else:
-                    layers = [
-                        kl.Flatten(),
-                        value_block_config.create_block_tf(),
-                    ]
-                    self.in_multi_layers.append(layers)
+
+def create_in_block_out_multi(
+    value_block_config: MLPBlockConfig,
+    image_block_config: ImageBlockConfig,
+    observation_space: SpaceBase,
+    enable_time_distributed_layer: bool = False,
+) -> Tuple[keras.Model, List[MultiVariableTypes]]:
+    if isinstance(observation_space, MultiSpace):
+        o = InputMultiBlock(
+            value_block_config,
+            image_block_config,
+            observation_space,
+            enable_time_distributed_layer,
+            is_image_flatten=False,
+            is_concat=False,
+        )
+        return o, o.out_types
+    elif observation_space.rl_type == RLTypes.IMAGE:
+        return InputImageBlock(
+            image_block_config,
+            observation_space,
+            enable_time_distributed_layer,
+            is_flatten=False,
+            out_multi=True,
+        ), [MultiVariableTypes.IMAGE]
+    else:
+        return InputValueBlock(value_block_config, out_multi=True), [MultiVariableTypes.VALUE]
+
+
+def create_in_block_out_value(
+    value_block_config: MLPBlockConfig,
+    image_block_config: ImageBlockConfig,
+    observation_space: SpaceBase,
+    enable_time_distributed_layer: bool = False,
+) -> keras.Model:
+    if isinstance(observation_space, MultiSpace):
+        return InputMultiBlock(
+            value_block_config,
+            image_block_config,
+            observation_space,
+            enable_time_distributed_layer,
+        )
+    elif observation_space.rl_type == RLTypes.IMAGE:
+        return InputImageBlock(
+            image_block_config,
+            observation_space,
+            enable_time_distributed_layer,
+        )
+    else:
+        return InputValueBlock(value_block_config)
+
+
+def create_in_block_out_image(
+    image_block_config: Optional[ImageBlockConfig],
+    observation_space: SpaceBase,
+    enable_time_distributed_layer: bool = False,
+) -> keras.Model:
+    if observation_space.rl_type == RLTypes.IMAGE:
+        assert image_block_config is not None
+        return InputImageBlock(
+            image_block_config,
+            observation_space,
+            enable_time_distributed_layer,
+            is_flatten=False,
+        )
+    else:
+        raise NotSupportedError(observation_space.rl_type)
+
+
+# -------------
+
+
+def create_input_image_layers(obs_space: SpaceBase):
+    err_msg = f"unknown observation_type: {obs_space}"
+    layers = []
+
+    if obs_space.env_type == EnvTypes.GRAY_2ch:
+        if len(obs_space.shape) == 2:
+            # (h, w) -> (h, w, 1)
+            layers.append(kl.Reshape(obs_space.shape + (1,)))
+        elif len(obs_space.shape) == 3:
+            # (len, h, w) -> (h, w, len)
+            layers.append(kl.Permute((2, 3, 1)))
         else:
-            self.is_multi_input = False
-            if observation_space.rl_type == RLTypes.IMAGE:
-                self.in_layers = self._create_input_image_layers(observation_space)
-                self.in_layers.append(image_block_config.create_block_tf(enable_time_distributed_layer))
-                self.in_layers.append(kl.Flatten())
-            else:
-                self.in_layers = [
-                    kl.Flatten(),
-                    value_block_config.create_block_tf(),
-                ]
+            raise TFLayerError(err_msg)
 
-    def _create_input_image_layers(self, obs_space: SpaceBase):
-        err_msg = f"unknown observation_type: {obs_space}"
-        layers = []
+    elif obs_space.env_type == EnvTypes.GRAY_3ch:
+        assert obs_space.shape[-1] == 1
+        if len(obs_space.shape) == 3:
+            # (h, w, 1)
+            pass
+        elif len(obs_space.shape) == 4:
+            # (len, h, w, 1) -> (len, h, w)
+            # (len, h, w) -> (h, w, len)
+            layers.append(kl.Reshape(obs_space.shape[:3]))
+            layers.append(kl.Permute((2, 3, 1)))
+        else:
+            raise TFLayerError(err_msg)
 
-        if obs_space.env_type == EnvTypes.GRAY_2ch:
-            if len(obs_space.shape) == 2:
-                # (h, w) -> (h, w, 1)
-                layers.append(kl.Reshape(obs_space.shape + (1,)))
-            elif len(obs_space.shape) == 3:
-                # (len, h, w) -> (h, w, len)
-                layers.append(kl.Permute((2, 3, 1)))
-            else:
-                raise TFLayerError(err_msg)
-
-        elif obs_space.env_type == EnvTypes.GRAY_3ch:
-            assert obs_space.shape[-1] == 1
-            if len(obs_space.shape) == 3:
-                # (h, w, 1)
-                pass
-            elif len(obs_space.shape) == 4:
-                # (len, h, w, 1) -> (len, h, w)
-                # (len, h, w) -> (h, w, len)
-                layers.append(kl.Reshape(obs_space.shape[:3]))
-                layers.append(kl.Permute((2, 3, 1)))
-            else:
-                raise TFLayerError(err_msg)
-
-        elif obs_space.env_type == EnvTypes.COLOR:
-            if len(obs_space.shape) == 3:
-                # (h, w, ch)
-                pass
-            else:
-                raise TFLayerError(err_msg)
-
-        elif obs_space.env_type == EnvTypes.IMAGE:
+    elif obs_space.env_type == EnvTypes.COLOR:
+        if len(obs_space.shape) == 3:
             # (h, w, ch)
             pass
         else:
-            raise UndefinedError(obs_space.env_type)
+            raise TFLayerError(err_msg)
 
-        return layers
+    elif obs_space.env_type == EnvTypes.IMAGE:
+        # (h, w, ch)
+        pass
+    else:
+        raise UndefinedError(obs_space.env_type)
+
+    return layers
+
+
+class InputValueBlock(KerasModelAddedSummary):
+    def __init__(self, value_block_config: MLPBlockConfig, out_multi: bool = False):
+        super().__init__()
+        self.out_multi = out_multi
+
+        self.in_layers = [
+            kl.Flatten(),
+            value_block_config.create_block_tf(),
+        ]
 
     def call(self, x, training=False):
-        if self.is_multi_input:
-            _x = []
-            for i in range(self.in_multi_size):
-                _x2 = x[i]
-                for h in self.in_multi_layers[i]:
-                    _x2 = h(_x2)
-                _x.append(_x2)
-            x = tf.concat(_x, axis=-1)
-        else:
-            for h in self.in_layers:
-                x = h(x, training=training)
-        return x
+        for h in self.in_layers:
+            x = h(x, training=training)
+        return [x] if self.out_multi else x
+
+
+class InputImageBlock(KerasModelAddedSummary):
+    def __init__(
+        self,
+        image_block_config: ImageBlockConfig,
+        observation_space: SpaceBase,
+        enable_time_distributed_layer: bool = False,
+        is_flatten: bool = True,
+        out_multi: bool = False,
+    ):
+        super().__init__()
+        self.out_multi = out_multi
+
+        self.in_layers = create_input_image_layers(observation_space)
+        self.in_layers.append(image_block_config.create_block_tf(enable_time_distributed_layer))
+        if is_flatten:
+            self.in_layers.append(kl.Flatten())
+
+    def call(self, x, training=False):
+        for h in self.in_layers:
+            x = h(x, training=training)
+        return [x] if self.out_multi else x
+
+
+class InputMultiBlock(KerasModelAddedSummary):
+    def __init__(
+        self,
+        value_block_config: Optional[MLPBlockConfig],
+        image_block_config: Optional[ImageBlockConfig],
+        observation_space: MultiSpace,
+        enable_time_distributed_layer: bool = False,
+        is_image_flatten: bool = True,
+        is_concat: bool = True,
+    ):
+        super().__init__()
+        self.is_concat = is_concat
+
+        self.in_indices = []
+        self.in_layers = []
+        self.out_types = []
+        for i, space in enumerate(observation_space.spaces):
+            if space.rl_type == RLTypes.IMAGE:
+                if image_block_config is None:
+                    logger.info("image space is skip")
+                    continue
+                layers = create_input_image_layers(space)
+                layers.append(image_block_config.create_block_tf(enable_time_distributed_layer))
+                if is_image_flatten:
+                    layers.append(kl.Flatten())
+                self.in_indices.append(i)
+                self.in_layers.append(layers)
+                self.out_types.append(MultiVariableTypes.IMAGE)
+            else:
+                if value_block_config is None:
+                    logger.info("value space is skip")
+                    continue
+                layers = [
+                    kl.Flatten(),
+                    value_block_config.create_block_tf(),
+                ]
+                self.in_indices.append(i)
+                self.in_layers.append(layers)
+                self.out_types.append(MultiVariableTypes.VALUE)
+        assert len(self.in_indices) > 0
+
+    def call(self, x, training=False):
+        x_arr = []
+        i = -1
+        for idx in self.in_indices:
+            i += 1
+            _x = x[idx]
+            for h in self.in_layers[i]:
+                _x = h(_x, training=training)
+            x_arr.append(_x)
+        if self.is_concat:
+            x_arr = tf.concat(x_arr, axis=-1)
+        return x_arr

99dc0d5f62770e2dac757f29dd59467e599a6b40
poco_cpp <pocopococpp198@gmail.com>
2024-03-20 22:22:38 +0900

    [rl.processor] update: NormalizeProcessor update

---
+++ tests/quick/rl/processors/test_normaliza_processor.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pytest
+
+import srl
+import srl.envs.grid
+from srl.base.rl.config import DummyRLConfig
+from srl.base.spaces.array_continuous import ArrayContinuousSpace
+from srl.base.spaces.array_discrete import ArrayDiscreteSpace
+from srl.base.spaces.box import BoxSpace
+from srl.base.spaces.continuous import ContinuousSpace
+from srl.base.spaces.discrete import DiscreteSpace
+from srl.rl.processors.normalize_processor import NormalizeProcessor
+
+
+@pytest.mark.parametrize(
+    "space, true_space, state, true_state",
+    [
+        [DiscreteSpace(5), ContinuousSpace(-1, 2), 4, 2.0],
+        [ContinuousSpace(1, 4), ContinuousSpace(-1, 2), 4, 2],
+        [ArrayDiscreteSpace(2, 1, 4), ArrayContinuousSpace(2, -1, 2), [1, 4], [-1, 2]],
+        [ArrayContinuousSpace(2, 1, 4), ArrayContinuousSpace(2, -1, 2), [1, 4], [-1, 2]],
+        [
+            BoxSpace((2, 1), 1, 4),
+            BoxSpace((2, 1), -1, 2),
+            np.array([[1], [4]], np.float32),
+            np.array([[-1], [2]], np.float32),
+        ],
+    ],
+)
+def test_1(space, true_space, state, true_state):
+
+    processor = NormalizeProcessor((-1, 2))
+    env = srl.make_env("Grid")
+
+    # --- change space
+    new_space = processor.preprocess_observation_space(space, env, DummyRLConfig())
+    print(new_space)
+    assert new_space == true_space
+
+    # --- decode
+    new_state = processor.preprocess_observation(state, env)
+    print(new_state)
+    if isinstance(true_state, np.ndarray):
+        assert (new_state == true_state).all()
+    else:
+        assert new_state == true_state

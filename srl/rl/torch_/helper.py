0ad9185fedcd001ae7c85f4d7736ec023d5aba27
poco_cpp <pocopococpp198@gmail.com>
2024-04-12 20:28:12 +0900

    [rl.torch.helper] add: twohotを追加

--- srl/rl/torch_/helper.py
+++ srl/rl/torch_/helper.py
@@ -66,3 +66,30 @@ def model_backup(model: nn.Module, to_cpu: bool = False):
             return copy.deepcopy(model).to("cpu").state_dict()
     else:
         return model.state_dict()
+
+
+def twohot_encode(x: torch.Tensor, size: int, low: float, high: float, device) -> torch.Tensor:
+    x = x.clamp(low, high)
+    # 0-bins のサイズで正規化
+    x = (size - 1) * (x - low) / (high - low)
+    # 整数部:idx 小数部:weight
+    idx = x.floor().to(torch.int32)
+    w = (x - idx).unsqueeze(-1)
+
+    onehot = torch.eye(size, dtype=torch.float32).to(device)
+    onehot = torch.vstack([onehot, torch.zeros(size)])
+    onehot1 = onehot[idx]
+    onehot2 = onehot[idx + 1]
+    return onehot1 * (1 - w) + onehot2 * w
+
+
+def twohot_decode(x: torch.Tensor, size: int, low: float, high: float, device):
+    bins = torch.arange(0, size).to(device)
+    bins = bins.unsqueeze(0).tile((x.shape[0], 1))
+    x = x * bins
+    x = x.sum(1)
+    return (x / (size - 1)) * (high - low) + low
+
+
+def binary_onehot_decode(x: torch.Tensor):
+    return x[:, 0]

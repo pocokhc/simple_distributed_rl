21e074be7ef99f35f7530ff17ef8d33b28b10e52
poco_cpp <pocopococpp198@gmail.com>
2024-05-05 09:43:52 +0900

    [base.spaces] new: TextSpaceを追加

--- srl/base/spaces/text.py
+++ srl/base/spaces/text.py
@@ -20,16 +20,19 @@ class TextSpace(SpaceBase[str]):
         max_length: int,
         min_length: int = 0,
         sample_charset: str = alphanumeric,
+        padding: str = " ",
     ) -> None:
         self._max_length = max_length
         self._min_length = min_length
         self._sample_charset = sample_charset
+        self._padding = padding
 
+        assert len(self._sample_charset) > 0
         assert 0 <= min_length
         assert min_length <= max_length
 
     @property
-    def base_stype(self) -> SpaceTypes:
+    def stype(self) -> SpaceTypes:
         return SpaceTypes.DISCRETE
 
     @property
@@ -42,11 +45,14 @@ class TextSpace(SpaceBase[str]):
         text = [random.choice(charset) for _ in range(n)]
         return "".join(text)
 
+    def get_valid_actions(self, masks: List[str] = []) -> List[str]:
+        raise NotImplementedError("TODO")  # 組み合わせ爆発どうするか未定
+
     def sanitize(self, val: Any) -> str:
         if not isinstance(val, str):
             val = str(val)
-        if len(val) < self._min_length:
-            val += "".join([" " for _ in range(self._min_length - len(val))])
+        if len(val) < self._max_length:
+            val += "".join([" " for _ in range(self._max_length - len(val))])
         if len(val) > self._max_length:
             val = val[: self._max_length]
         return val
@@ -60,15 +66,25 @@ class TextSpace(SpaceBase[str]):
             return False
         return True
 
+    def to_str(self, val: str) -> str:
+        return val
+
     def get_default(self) -> str:
-        if self._min_length == 0:
+        if self._max_length == 0:
             return ""
-        return "".join([" " for _ in range(self._min_length)])
+        return "".join([self._sample_charset[0] for _ in range(self._max_length)])
 
     def copy(self) -> "TextSpace":
-        return TextSpace(self._min_length, self._max_length, self._sample_charset)
+        return TextSpace(
+            self._max_length,
+            self._min_length,
+            self._sample_charset,
+            self._padding,
+        )
 
     def __eq__(self, o: "TextSpace") -> bool:
+        if not isinstance(o, TextSpace):
+            return False
         return (
             self._min_length == o._min_length
             and self._max_length == o._max_length
@@ -78,44 +94,48 @@ class TextSpace(SpaceBase[str]):
     def __str__(self) -> str:
         return f"Text({self._min_length}, {self._max_length})"
 
+    # --- stack
+    def create_stack_space(self, length: int):
+        return TextSpace(
+            self._max_length * length,
+            self._min_length,
+            self._sample_charset,
+            self._padding,
+        )
+
+    def encode_stack(self, val: List[str]) -> str:
+        return "".join(val)
+
     # --------------------------------------
     # action discrete
     # --------------------------------------
     @property
     def int_size(self) -> int:
-        self._create_tbl()
-        assert self.decode_tbl is not None
-        return len(self.decode_tbl)
+        raise NotImplementedError("TODO")
 
     def encode_to_int(self, val: str) -> int:
-        self._create_tbl()
-        return self.encode_tbl[tuple(val)]
+        raise NotImplementedError("TODO")
 
     def decode_from_int(self, val: int) -> str:
-        self._create_tbl()
-        assert self.decode_tbl is not None
-        return list(self.decode_tbl[val])
+        raise NotImplementedError("TODO")
 
     # --------------------------------------
     # observation discrete
     # --------------------------------------
     @property
     def list_int_size(self) -> int:
-        """continuous list length"""
-        raise NotImplementedError()
+        return self._max_length
 
     @property
     def list_int_low(self) -> List[int]:
-        """continuous list length range"""
-        raise NotImplementedError()
+        return [0] * self._max_length
 
     @property
     def list_int_high(self) -> List[int]:
-        """continuous list length range"""
-        raise NotImplementedError()
+        return [0x7F] * self._max_length
 
     def encode_to_list_int(self, val: str) -> List[int]:
-        # 長さ揃えTODO
+        val = val + self._padding * (self._max_length - len(val))
         return [ord(c) for c in val]
 
     def decode_from_list_int(self, val: List[int]) -> str:
@@ -126,19 +146,19 @@ class TextSpace(SpaceBase[str]):
     # --------------------------------------
     @property
     def list_float_size(self) -> int:
-        return TODO
+        return self._max_length
 
     @property
     def list_float_low(self) -> List[float]:
-        return TODO
+        return [0.0] * self._max_length
 
     @property
     def list_float_high(self) -> List[float]:
-        return TODO
+        return [float(0x7F)] * self._max_length
 
     def encode_to_list_float(self, val: str) -> List[float]:
-        # 長さ揃えTODO
-        return [ord(c) for c in val]
+        val = val + self._padding * (self._max_length - len(val))
+        return [float(ord(c)) for c in val]
 
     def decode_from_list_float(self, val: List[float]) -> str:
         return "".join([chr(int(n)) for n in val])
@@ -148,15 +168,15 @@ class TextSpace(SpaceBase[str]):
     # --------------------------------------
     @property
     def np_shape(self) -> Tuple[int, ...]:
-        return (self.list_size,)
+        return (self._max_length,)
 
     @property
     def np_low(self) -> np.ndarray:
-        return np.array(self.list_low)
+        return np.array([0.0] * self._max_length)
 
     @property
     def np_high(self) -> np.ndarray:
-        return np.array(self.list_high)
+        return np.array([0x7F] * self._max_length)
 
     def encode_to_np(self, val: str, dtype) -> np.ndarray:
         return np.array(self.encode_to_list_float(val), dtype=dtype)
---
+++ tests/quick/base/spaces/test_text.py
@@ -0,0 +1,106 @@
+import numpy as np
+import pytest
+
+from srl.base.define import SpaceTypes
+from srl.base.spaces.text import TextSpace
+
+
+def test_space_basic():
+    space = TextSpace(5)
+
+    assert space.stype == SpaceTypes.DISCRETE
+    assert space.dtype == np.uint64
+
+    # --- check_val
+    assert space.check_val("a")
+    assert not space.check_val(0)
+
+    # --- to_str
+    assert space.to_str("a") == "a"
+
+    # --- get_default
+    assert space.get_default() == "aaaaa"
+
+    # --- copy
+    c = space.copy()
+    assert space == c
+
+    # --- stack
+    o = space.create_stack_space(3)
+    assert isinstance(o, TextSpace)
+    assert o == TextSpace(5 * 3)
+    v = space.encode_stack(["a", "b", "c"])
+    assert v == "abc"
+
+
+def test_space():
+    space = TextSpace(3)
+    print(space)
+
+    # --- discrete
+    with pytest.raises(NotImplementedError):
+        assert space.int_size == 5
+        de = space.decode_from_int(2)
+        assert isinstance(de, int)
+        assert de == 3
+        en = space.encode_to_int(3)
+        assert isinstance(en, int)
+        assert en == 2
+
+    # --- list int
+    assert space.list_int_size == 3
+    assert space.list_int_low == [0, 0, 0]
+    assert space.list_int_high == [0x7F, 0x7F, 0x7F]
+    en = space.encode_to_list_int("ab")
+    assert isinstance(en, list)
+    assert len(en) == 3
+    assert en[0] == ord("a")
+    assert en[1] == ord("b")
+    assert en[2] == ord(" ")
+    de = space.decode_from_list_int(en)
+    assert de == "ab "
+
+    # --- list float
+    assert space.list_float_size == 3
+    np.testing.assert_array_equal(space.list_float_low, [0, 0, 0])
+    np.testing.assert_array_equal(space.list_float_high, [0x7F, 0x7F, 0x7F])
+    en = space.encode_to_list_float("ab")
+    assert len(en) == 3
+    assert en[0] == ord("a")
+    assert en[1] == ord("b")
+    assert en[2] == ord(" ")
+    de = space.decode_from_list_float(en)
+    assert de == "ab "
+
+    # --- continuous numpy
+    assert space.np_shape == (3,)
+    np.testing.assert_array_equal(space.np_low, [0, 0, 0])
+    np.testing.assert_array_equal(space.np_high, [0x7F, 0x7F, 0x7F])
+    en = space.encode_to_np("ab", np.float32)
+    assert isinstance(en, np.ndarray)
+    assert len(en) == 3
+    assert en[0] == ord("a")
+    assert en[1] == ord("b")
+    assert en[2] == ord(" ")
+    de = space.decode_from_np(en)
+    assert de == "ab "
+
+    # --- sample
+    actions = [space.sample() for _ in range(100)]
+    print(actions)
+
+    # --- eq
+    assert space == TextSpace(3)
+    assert space != TextSpace(4)
+    assert space != "a"
+
+
+def test_sanitize():
+    space = TextSpace(3)
+
+    val = space.sanitize("ab")
+    assert space.check_val(val)
+    assert val == "ab "
+
+    assert not space.check_val(1)
+    assert not space.check_val("abcd")

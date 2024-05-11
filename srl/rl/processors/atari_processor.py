a6c1ae9f6b309d139414df79c4ebcb10479fbac4
poco_cpp <pocopococpp198@gmail.com>
2024-05-11 09:36:41 +0900

    [rl.processors] new: AtariProcessor update

--- srl/rl/processors/atari_processor.py
+++ srl/rl/processors/atari_processor.py
@@ -1,26 +1,56 @@
+import numpy as np
+
+from srl.base.define import DoneTypes, SpaceTypes
 from srl.base.env.env_run import EnvRun
-from srl.base.rl.config import RLConfig
-from srl.base.rl.processor import EpisodeProcessor
+from srl.base.env.processor import Processor
+from srl.base.spaces.box import BoxSpace
+from srl.base.spaces.space import SpaceBase
+from srl.rl.functions import image_processor
 from srl.utils.common import is_package_installed
 
 
-class AtariProcessor(EpisodeProcessor):
+class AtariProcessor(Processor):
     def __init__(self):
         assert is_package_installed("ale_py")
 
-    def setup(self, env: EnvRun, rl_config: RLConfig):
-        self.enable = "AtariEnv" in str(env.unwrapped)
-
-    def on_reset(self, env: EnvRun):
-        if not self.enable:
-            return
+    def remap_reset(self, env: EnvRun):
         self.lives = env.unwrapped.env.unwrapped.ale.lives()
 
-    def preprocess_done(self, done: bool, env: EnvRun) -> bool:
-        if not self.enable:
-            return done
+    def remap_step(self, state, rewards, done, info, env: EnvRun):
         new_lives = env.unwrapped.env.unwrapped.ale.lives()
         if new_lives < self.lives:
-            return True
+            return state, rewards, DoneTypes.TERMINATED, info
         self.lives = new_lives
-        return done
+        return state, rewards, done, info
+
+
+class AtariPongProcessor(Processor):
+    def __init__(self):
+        assert is_package_installed("ale_py")
+
+    def remap_observation_space(self, observation_space: SpaceBase, env: EnvRun) -> SpaceBase:
+        return BoxSpace((84, 84), 0, 1, stype=SpaceTypes.GRAY_2ch)
+
+    def remap_reset(self, state, info, env: EnvRun):
+        self.point = 0
+        return self._remap_state(state), info
+
+    def remap_step(self, state, rewards, done, info, env: EnvRun):
+        if env.reward == 1:
+            self.point += 1
+        elif env.reward == -1:
+            self.point += 1
+        if self.point == 5:
+            done = DoneTypes.TERMINATED
+        return self._remap_state(state), rewards, done, info
+
+    def _remap_state(self, state):
+        state = image_processor(
+            state,
+            SpaceTypes.COLOR,
+            SpaceTypes.GRAY_2ch,
+            trimming=(35, 10, 195, 150),
+            resize=(84, 84),
+        ).astype(np.float64)
+        state = np.where(state > 127, 1.0, 0.0)
+        return state

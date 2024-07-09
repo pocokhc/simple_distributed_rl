e6f2b9d9ad1e2ede1f3dfea788cc2d4155e10d89
poco_cpp <pocopococpp198@gmail.com>
2024-07-09 18:15:21 +0900

    [base.system.device] fix: tf-gpu(古いtfバージョン)に対してCPUに変更して実行するとpythonプロセス自体が落ちる不具合対応

--- srl/base/system/device.py
+++ srl/base/system/device.py
@@ -26,7 +26,10 @@ def setup_device(
         return "/CPU", "cpu"
     if framework == "tf":
         framework = "tensorflow"
-    assert framework in ["tensorflow", "torch"], "Framework can specify 'tensorflow' or 'torch'."
+    assert framework in [
+        "tensorflow",
+        "torch",
+    ], "Framework can specify 'tensorflow' or 'torch'."
 
     if __setup_device:
         if __framework != framework:
@@ -43,6 +46,19 @@ def setup_device(
         logger.info(f"{log_prefix}[device] CUDA_VISIBLE_DEVICES is not define.")
 
     # --- CUDA_VISIBLE_DEVICES ---
+    # tf-gpuはCUDA_VISIBLE_DEVICESでGPUを消すと一定学習後プロセス自体が起こる
+    # 初期化時に "failed call to cuInit: CUDA_ERROR_NO_DEVICE: no CUDA-capable device is detected" の出力あり
+    # なのでtf-gpuはCUDA_VISIBLE_DEVICESで制御せず、tf.device側に任せる
+    v216_older = False
+    if framework == "tensorflow":
+        import tensorflow as tf
+
+        from srl.utils.common import compare_less_version
+
+        v216_older = compare_less_version(tf.__version__, "2.16.0")
+        if v216_older:
+            set_CUDA_VISIBLE_DEVICES_if_CPU = False
+
     if set_CUDA_VISIBLE_DEVICES_if_CPU:
         if "CPU" in device:
             os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
@@ -55,8 +71,9 @@ def setup_device(
     # -----------------------------
 
     # --- tf memory growth ---
-    # Tensorflow,GPU がある場合に実施(CPUにしてもなぜかGPUの初期化は走る場合あり)
-    if framework == "tensorflow" and tf_enable_memory_growth:
+    # tf-gpuはCPUでもGPUがあるため実行
+    # 新しいバージョンはCPUのみならエラーではなく[]がちゃんと返る
+    if tf_enable_memory_growth and framework == "tensorflow":
         try:
             import tensorflow as tf
 
@@ -71,58 +88,50 @@ def setup_device(
             raise
     # -----------------------
 
+    used_device_tf = "/CPU"
+    used_device_torch = "cpu"
     if "CPU" in device:
-        # --- CPU ---
         if "CPU:" in device:
             t = device.split(":")
             used_device_tf = f"/CPU:{t[1]}"
             used_device_torch = f"cpu:{t[1]}"
-        else:
+    elif framework == "tensorflow":
+        # --- tensorflow GPU/AUTO check
+        import tensorflow as tf
+
+        gpu_devices = tf.config.list_physical_devices("GPU")
+        if len(gpu_devices) == 0:
+            if "GPU" in device:
+                logger.warning(f"{log_prefix}[device] (tf) GPU is not found. {tf.config.list_physical_devices()}")
+
             used_device_tf = "/CPU"
+
+        else:
+            logger.info(f"{log_prefix}[device] (tf) gpu device: {len(gpu_devices)}")
+
+            if "GPU:" in device:
+                t = device.split(":")
+                used_device_tf = f"/GPU:{t[1]}"
+            else:
+                used_device_tf = "/GPU"
+
+    elif framework == "torch":
+        # --- torch GPU/AUTO check
+        import torch
+
+        if torch.cuda.is_available():
+            logger.info(f"{log_prefix}[device] (torch) gpu device: {torch.cuda.get_device_name()}")
+
+            if "GPU:" in device:
+                t = device.split(":")
+                used_device_torch = f"cuda:{t[1]}"
+            else:
+                used_device_torch = "cuda"
+        else:
+            if "GPU" in device:
+                logger.warning(f"{log_prefix}[device] (torch) GPU is not found.")
+
             used_device_torch = "cpu"
-    else:
-        used_device_tf = "/CPU"
-        used_device_torch = "cpu"
-
-        # --- GPU (AUTOの場合もあり) ---
-        if framework == "tensorflow":
-            # --- tensorflow GPU check
-            import tensorflow as tf
-
-            gpu_devices = tf.config.list_physical_devices("GPU")
-            if len(gpu_devices) == 0:
-                if "GPU" in device:
-                    logger.warning(f"{log_prefix}[device] (tf) GPU is not found. {tf.config.list_physical_devices()}")
-
-                used_device_tf = "/CPU"
-
-            else:
-                logger.info(f"{log_prefix}[device] (tf) gpu device: {len(gpu_devices)}")
-
-                if "GPU:" in device:
-                    t = device.split(":")
-                    used_device_tf = f"/GPU:{t[1]}"
-                else:
-                    used_device_tf = "/GPU"
-
-        if framework == "torch":
-            # --- torch GPU check
-            import torch
-
-            if torch.cuda.is_available():
-                logger.info(f"{log_prefix}[device] (torch) gpu device: {torch.cuda.get_device_name()}")
-
-                if "GPU:" in device:
-                    t = device.split(":")
-                    used_device_torch = f"cuda:{t[1]}"
-                else:
-                    used_device_torch = "cuda"
-            else:
-                if "GPU" in device:
-                    logger.warning(f"{log_prefix}[device] (torch) GPU is not found.")
-
-                used_device_torch = "cpu"
-    # -------------------------
 
     __setup_device = True
     __framework = framework

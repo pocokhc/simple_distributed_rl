a7097a386f82db6be98839ede31d421245fd6b29
poco_cpp <pocopococpp198@gmail.com>
2025-01-17 17:58:49 +0900

    [examples.raw] update: single playのサンプルも追加

--- examples/raw/play_2player.py
+++ examples/raw/play_2player.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 import numpy as np
 
@@ -16,13 +16,13 @@ from srl.algorithms import ql  # isort: skip
 
 def _run_episode(
     env: EnvRun,
-    worker: WorkerRun,
+    workers: List[WorkerRun],
     trainer: Optional[RLTrainer],
     rendering: bool,
 ):
     # 1. reset
     env.reset()
-    worker.on_reset(player_index=0)
+    [w.on_reset(player_index=i) for i, w in enumerate(workers)]
 
     # --- env render
     if rendering:
@@ -31,21 +31,21 @@ def _run_episode(
 
     while not env.done:
         # 2. action
-        action = worker.policy()
+        action = workers[env.next_player].policy()
 
         # --- worker render
         if rendering:
             print(f"player {env.next_player}")
-            worker.render()
+            workers[env.next_player].render()
 
         # 3. step
         env.step(action)
-        worker.on_step()
+        [w.on_step() for w in workers]
 
         # --- env render
         if rendering:
             print("--- turn {}, action {}, rewards: {}, done: {}, next player {}, info: {}, ".format(env.step_num, action, env.rewards, env.done, env.next_player, env.info))
-            print("player {} info: {}".format(env.next_player, worker.info))
+            print("player {} info: {}".format(env.next_player, workers[env.next_player].info))
             env.render()
 
         # 4. train
@@ -60,7 +60,8 @@ def _run_episode(
             print(env.reward)
             print(env.done)
             print(env.info)
-            print(worker.info)
+            print(workers[0].info)
+            print(workers[1].info)
             print(train_info)
 
     if rendering:
@@ -70,7 +71,7 @@ def _run_episode(
 
 
 def main():
-    env_config = srl.EnvConfig("Grid")
+    env_config = srl.EnvConfig("OX")
     rl_config = ql.Config()
 
     # --- make instance
@@ -79,40 +80,85 @@ def main():
     parameter = rl_config.make_parameter()
     memory = rl_config.make_memory()
     trainer = rl_config.make_trainer(parameter, memory)
-    worker = rl_config.make_worker(env, parameter, memory)
+    workers = [
+        rl_config.make_worker(env, parameter, memory),
+        srl.make_worker("random", env),
+    ]
 
     # --- train
     context = srl.RunContext(env_config, rl_config, training=True)
     env.setup()
-    worker.on_start(context)
+    [w.on_start(context) for w in workers]
     trainer.on_start(context)
     for episode in range(10000):
-        step, rewards = _run_episode(env, worker, trainer, rendering=False)
+        step, rewards = _run_episode(env, workers, trainer, rendering=False)
         if episode % 1000 == 0:
             print(f"{episode} / 10000 episode, {step} step, {rewards} reward")
-    worker.on_end()
+    [w.on_end() for w in workers]
     trainer.on_end()
 
     # --- evaluate
     context = srl.RunContext(env_config, rl_config)
     env.setup()
-    worker.on_start(context)
+    [w.on_start(context) for w in workers]
     rewards_list = []
     for episode in range(100):
-        _, rewards = _run_episode(env, worker, None, rendering=False)
+        _, rewards = _run_episode(env, workers, None, rendering=False)
         rewards_list.append(rewards)
     print(f"Average reward for 100 episodes: {np.mean(rewards_list, axis=0)}")
-    worker.on_end()
+    [w.on_end() for w in workers]
 
     # --- render
     context = srl.RunContext(env_config, rl_config, render_mode="terminal")
     env.setup(context.render_mode)
-    worker.on_start(context)
-    _run_episode(env, worker, None, rendering=True)
-    worker.on_end()
+    [w.on_start(context) for w in workers]
+    _run_episode(env, workers, None, rendering=True)
+    [w.on_end() for w in workers]
+
+
+def play_cpu():
+    import srl.rl.human
+
+    env_config = srl.EnvConfig("OX")
+
+    # make
+    env = env_config.make()
+    player = srl.make_worker("human", env)
+    cpu = env.make_worker("cpu")
+    assert cpu is not None
+
+    # set context
+    context = srl.RunContext(env_config, render_mode="terminal")
+    env.setup(context.render_mode)
+    player.on_start(context)
+    cpu.on_start(context)
+
+    # reset
+    env.reset()
+    player.on_reset(player_index=0)
+    cpu.on_reset(player_index=1)
+
+    env.render()
+
+    while not env.done:
+        # 1 step
+        if env.next_player == 0:
+            action = player.policy()
+        else:
+            action = cpu.policy()
+        env.step(action)
+        player.on_step()
+        cpu.on_step()
+
+        env.render()
+
+    # end
+    player.on_end()
+    cpu.on_end()
 
 
 if __name__ == "__main__":
     common.logger_print()
 
     main()
+    play_cpu()

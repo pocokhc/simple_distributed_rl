6152a66c2ec8943d5ac87473948818d61181ccb0
poco_cpp <pocopococpp198@gmail.com>
2025-02-05 19:20:46 +0900

    [examples.baseline] update

--- examples/baseline/gym/toy_text/cliff_walking.py
+++ examples/baseline/gym/toy_text/cliff_walking.py
@@ -3,22 +3,25 @@ import os
 import numpy as np
 
 import srl
-from srl.algorithms import ql, search_dynaq
+from srl.algorithms import go_dynaq, ql, search_dynaq
 
 base_dir = os.path.dirname(__file__)
-BASE_LR = 0.02
-TRAIN_TIMEOUT = 10
+BASE_LR = 0.01
+TRAIN_COUNT = 100_000
 ENV_PRE = "cw"
 
 
 def _train(name, rl_config):
-    runner = srl.Runner("CliffWalking-v0", rl_config)
+    runner = srl.Runner(
+        srl.EnvConfig("CliffWalking-v0", max_episode_steps=100),
+        rl_config,
+    )
     runner.set_history_on_file(
         os.path.join(base_dir, f"_{ENV_PRE}_{name}"),
         enable_eval=True,
         eval_episode=1,
     )
-    runner.train(timeout=TRAIN_TIMEOUT)
+    runner.train(max_steps=TRAIN_COUNT)
     rewards = runner.evaluate(max_episodes=10)
     print(f"evaluate episodes: {np.mean(rewards)} > -20")
 
@@ -31,18 +34,24 @@ def main_search_dynaq():
     _train("SearchDynaQ", search_dynaq.Config(q_ext_lr=BASE_LR, q_int_lr=BASE_LR))
 
 
+def main_go_dynaq():
+    _train("GoDynaQ", go_dynaq.Config(q_ext_lr=BASE_LR, q_int_lr=BASE_LR))
+
+
 def compare():
     histories = srl.Runner.load_histories(
         [
             os.path.join(base_dir, f"_{ENV_PRE}_QL"),
             os.path.join(base_dir, f"_{ENV_PRE}_SearchDynaQ"),
+            os.path.join(base_dir, f"_{ENV_PRE}_GoDynaQ"),
         ]
     )
-    histories.plot("train", "eval_reward0", title=f"TrainTime:{TRAIN_TIMEOUT}s, lr={BASE_LR}")
-    histories.plot("time", "eval_reward0", title=f"TrainTime:{TRAIN_TIMEOUT}s, lr={BASE_LR}")
+    histories.plot("train", "eval_reward0", title=f"Train:{TRAIN_COUNT}s, lr={BASE_LR}")
+    histories.plot("time", "eval_reward0", title=f"Train:{TRAIN_COUNT}s, lr={BASE_LR}")
 
 
 if __name__ == "__main__":
     main_ql()
     main_search_dynaq()
+    main_go_dynaq()
     compare()

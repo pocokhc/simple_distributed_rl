241b8b995b31a0135e9a72991af55defec83dc88
poco_cpp <pocopococpp198@gmail.com>
2025-04-14 18:39:29 +0900

    [envs.processors.atari_processor] update: 引数を追加し更新

--- srl/envs/processors/atari_processor.py
+++ srl/envs/processors/atari_processor.py
@@ -8,27 +8,52 @@ from srl.base.env.processor import EnvProcessor
 from srl.base.spaces.box import BoxSpace
 from srl.base.spaces.space import SpaceBase
 from srl.rl.functions import image_processor
-from srl.utils.common import is_package_installed
 
 
 class AtariProcessor(EnvProcessor):
-    def __init__(self):
-        assert is_package_installed("ale_py")
+    def __init__(
+        self,
+        terminal_on_life_loss: bool = False,
+        resize=(84, 84),
+        grayscale: bool = True,
+        binarize: bool = True,
+    ):
+        import ale_py  # include atari gym # noqa: F401
 
-    def remap_reset(self, state, env_run: EnvRun, **kwargs):
+        self.terminal_on_life_loss = terminal_on_life_loss
+        self.resize = resize
+        self.space_type = SpaceTypes.GRAY_2ch if grayscale else SpaceTypes.COLOR
+        self.binarize = binarize
+
+    def remap_observation_space(self, prev_space: SpaceBase, **kwargs) -> SpaceBase:
+        return BoxSpace(self.resize, 0, 255, np.uint8, stype=self.space_type)
+
+    def remap_observation(self, state, prev_space: SpaceBase, new_space: SpaceBase, **kwargs):
+        state = image_processor(
+            state,
+            SpaceTypes.COLOR,
+            self.space_type,
+            resize=self.resize,
+        )
+        if self.binarize:
+            state = np.where(state > 127, 255, 0).astype(np.uint8)
+        return state
+
+    def remap_reset(self, env_run: EnvRun, **kwargs):
         self.lives = env_run.unwrapped.env.unwrapped.ale.lives()
 
     def remap_step(self, rewards: List[float], terminated: bool, truncated: bool, env_run: EnvRun, **kwargs):
-        new_lives = env_run.unwrapped.env.unwrapped.ale.lives()
-        if new_lives < self.lives:
-            return rewards, True, truncated
-        self.lives = new_lives
+        if self.terminal_on_life_loss:
+            new_lives = env_run.unwrapped.env.unwrapped.ale.lives()
+            if new_lives < self.lives:
+                return rewards, True, truncated
+            self.lives = new_lives
         return rewards, terminated, truncated
 
 
 class AtariPongProcessor(EnvProcessor):
     def __init__(self):
-        assert is_package_installed("ale_py")
+        import ale_py  # include atari gym  # noqa: F401
 
     def remap_observation_space(self, prev_space: SpaceBase, **kwargs) -> SpaceBase:
         return BoxSpace((84, 84), 0, 255, np.uint8, stype=SpaceTypes.GRAY_2ch)

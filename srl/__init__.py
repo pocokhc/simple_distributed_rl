12161fb462528d8f9b97cbd1be3c8abdb2cfdd9e
poco_cpp <pocopococpp198@gmail.com>
2025-04-29 15:06:45 +0900

    [slr] change: pypi登録用に準備

--- srl/__init__.py
+++ srl/__init__.py
@@ -4,7 +4,7 @@ from .base.env.registration import make as make_env  # noqa F401
 from .base.rl.registration import make_worker  # noqa F401
 from .base.rl.registration import make_workers  # noqa F401
 from .runner.runner import Runner  # noqa F401
-from .version import VERSION as __version__  # noqa F401
+from .version import __version__  # noqa F401
 
 __all__ = [
     "base",

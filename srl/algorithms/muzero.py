c8be1cc7d574c8f93680f234edc3d092363ed0f0
poco_cpp <pocopococpp198@gmail.com>
2025-05-06 19:55:21 +0900

    [base.rl.config] del: RLConfigからenv関係の情報を削除（env_max_episode_steps,env_player_num）

--- srl/algorithms/muzero.py
+++ srl/algorithms/muzero.py
@@ -516,6 +516,7 @@ class Trainer(RLTrainer[Config, Parameter, Memory]):
 # ------------------------------------------------------
 class Worker(RLWorker[Config, Parameter, Memory]):
     def on_setup(self, worker, context) -> None:
+        self.env_player_num = worker.env.player_num
         self.policy_tau_sch = self.config.policy_tau_scheduler.create(self.config.policy_tau)
         self._v_min = np.inf
         self._v_max = -np.inf
@@ -591,7 +592,7 @@ class Worker(RLWorker[Config, Parameter, Memory]):
             self.config.v_min,
             self.config.v_max,
         )
-        enemy_turn = self.config.env_player_num > 1  # 2player以上は相手番と決め打ち
+        enemy_turn = self.env_player_num > 1  # 2player以上は相手番と決め打ち
 
         if self.N[state_str][action] == 0:
             # leaf node ならロールアウト

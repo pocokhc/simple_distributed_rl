f7942c9c172f12ff128200706d1a81b34ea063f4
poco_cpp <pocopococpp198@gmail.com>
2025-05-16 21:29:12 +0900

    [base.rl.algorithms.extend_worker] fix: 更新が遅れていたので更新

--- srl/base/rl/algorithms/extend_worker.py
+++ srl/base/rl/algorithms/extend_worker.py
@@ -18,6 +18,24 @@ class ExtendWorker(RLWorker):
         super().__init__(config, parameter, memory)
         self.base_worker = worker
 
+    def on_setup(self, worker, context) -> None:
+        self.base_worker.on_setup(worker, context)
+
+    def on_teardown(self, worker) -> None:
+        self.base_worker.on_teardown(worker)
+
+    def on_reset(self, worker) -> None:
+        self.base_worker.on_reset(worker)
+
+    def on_step(self, worker) -> None:
+        self.base_worker.on_step(worker)
+
+    def render_terminal(self, worker, **kwargs) -> None:
+        self.base_worker.render_terminal(worker, **kwargs)
+
+    def render_rgb_array(self, worker, **kwargs):
+        return self.base_worker.render_rgb_array(worker, **kwargs)
+
     def _set_worker_run(self, worker: WorkerRun):
         super()._set_worker_run(worker)
         self.base_worker._set_worker_run(worker)

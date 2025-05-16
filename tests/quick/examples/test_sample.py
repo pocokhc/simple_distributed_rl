fae2fc20d9f3161e6ebcf8ba12c50867af6831b4
poco_cpp <pocopococpp198@gmail.com>
2025-05-16 21:16:30 +0900

    mlflow test移動忘れ

--- tests/quick/examples/test_sample.py
+++ tests/quick/examples/test_sample.py
@@ -33,15 +33,3 @@ def test_sample_long_training():
 
     sample_long_training.train(timeout=1)
     sample_long_training.evaluate()
-
-
-def test_sample_mlflow():
-    pytest.skip("環境に左右されるのでskip")
-
-    wkdir = setup_examples_test(add_path="")
-
-    import sample_mlflow  # type: ignore
-
-    sample_mlflow.train_ql(timeout=1)
-    sample_mlflow.load_ql_parameter()
-    sample_mlflow.train_vanilla_policy(timeout=1)

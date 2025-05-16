967a014cd110adf6b76651bf108df19bae77904d
poco_cpp <pocopococpp198@gmail.com>
2025-05-16 20:06:11 +0900

    [tests.mlflow] move: mlflowのテストを一まとめに移動

---
+++ tests/quick/mlflow_/examples/test_sample_mlflow.py
@@ -0,0 +1,11 @@
+from tests.quick.examples.examples_common import setup_examples_test
+
+
+def test_sample_mlflow():
+    wkdir = setup_examples_test(add_path="")
+
+    import sample_mlflow  # type: ignore
+
+    sample_mlflow.train_ql(timeout=1)
+    sample_mlflow.load_ql_parameter()
+    sample_mlflow.train_vanilla_policy(timeout=1)

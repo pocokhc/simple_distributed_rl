36484ede7c96dbe3595261ad20b688c70f34427f
poco_cpp <pocopococpp198@gmail.com>
2025-06-21 20:03:14 +0900

    mp関係を別フォルダに移動

--- tests/quick/examples/test_raw.py
+++ tests/quick/examples/test_raw.py
@@ -42,33 +42,3 @@ def test_play_use_run():
     import play_use_run  # type: ignore
 
     play_use_run.main()
-
-
-is_github_actions: bool = os.getenv("GITHUB_ACTIONS") == "true"
-
-
-@pytest.mark.skipif(is_github_actions, reason="mpが不安定なため")
-def test_play_mp():
-    wkdir = setup_examples_test(add_path="raw")
-
-    import play_mp  # type: ignore
-
-    play_mp.main()
-
-
-@pytest.mark.skipif(is_github_actions, reason="mpが不安定なため")
-def test_play_mp_memory():
-    wkdir = setup_examples_test(add_path="raw")
-
-    import play_mp_memory  # type: ignore
-
-    play_mp_memory.main()
-
-
-@pytest.mark.skipif(is_github_actions, reason="mpが不安定なため")
-def test_play_mp_no_queue():
-    wkdir = setup_examples_test(add_path="raw")
-
-    import play_mp_no_queue  # type: ignore
-
-    play_mp_no_queue.main()
--- tests/quick/runner/test_runner_play.py
+++ tests/quick/runner/test_runner_play.py
@@ -27,17 +27,13 @@ def test_train():
     assert state.train_count == 10
 
 
-@pytest.mark.parametrize("enable_mp_memory", [False, True])
-def test_train_multi_runner(enable_mp_memory):
+def test_train_multi_runner():
     runner1 = srl.Runner("Grid", ql.Config())
     runner2 = srl.Runner("OX", ql.Config())
     runner3 = srl.Runner("OX", ql.Config())
 
-    runner1.train_mp(max_train_count=10, enable_mp_memory=enable_mp_memory)
     runner1.train(max_train_count=10)
-
     runner3.train(max_train_count=10)
-    runner3.train_mp(max_train_count=10, enable_mp_memory=enable_mp_memory)
     runner2.train(max_train_count=10)
     runner1.train(max_train_count=10)
 

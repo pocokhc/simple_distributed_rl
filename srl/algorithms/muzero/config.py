8d87c386ebdd06ab904064ba08f1ad7b7b8be068
poco_cpp <pocopococpp198@gmail.com>
2025-08-12 19:55:25 +0900

    bug fix

--- srl/algorithms/muzero/config.py
+++ srl/algorithms/muzero/config.py
@@ -131,7 +131,7 @@ class Config(RLConfig):
 
     def get_processors(self, prev_observation_space: SpaceBase) -> List[RLProcessor]:
         if prev_observation_space.is_image():
-            return self.input_image_block.processors
+            return self.input_image_block.get_processors()
         raise ValueError(f"The input supports only image format. {prev_observation_space}")
 
     def get_name(self) -> str:

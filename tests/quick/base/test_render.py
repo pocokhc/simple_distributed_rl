17520f56b6b2d9bda3afc859f5647faeb75b6351
poco_cpp <pocopococpp198@gmail.com>
2026-04-23 00:37:38 +0900

    [tests.base.test_render] update: 更新忘れ

--- tests/quick/base/test_render.py
+++ tests/quick/base/test_render.py
@@ -11,32 +11,17 @@ class StubRender(IRenderer):
     def render_terminal(self, text, **kwargs) -> None:
         print(text)
 
-    def render_rgb_array(self, return_rgb, **kwargs) -> Optional[np.ndarray]:
-        if return_rgb:
-            return np.zeros((4, 4, 3))
-        else:
-            return None
+    def render_rgb_array(self, **kwargs) -> Optional[np.ndarray]:
+        return np.zeros((4, 4, 3))
 
 
-def test_render_terminal():
-    render = Render(StubRender())
-    render.set_render_mode(mode="terminal")
-
-    text = "StubRender\nAAA"
-    render.render(text=text)
-
-    text2 = render.get_cached_terminal_text(text=text)
+def _assert_terminal_text(render, text):
+    text2 = render.get_terminal_text()
     assert text2 == text + "\n"
 
 
-def test_render_terminal_to_image():
-    pytest.importorskip("pygame")
-    pytest.importorskip("PIL")
-    render = Render(StubRender())
-    render.set_render_mode(mode="rgb_array")
-
-    text = "StubRender\nAAA"
-    img = render.get_cached_terminal_text_to_image(text=text)
+def _assert_terminal_rgb_array(render):
+    img = render.get_terminal_rgb_array()
     assert img is not None
     assert len(img.shape) == 3
     assert img.shape[2] == 3
@@ -45,37 +30,84 @@ def test_render_terminal_to_image():
     assert img.dtype == np.uint8
 
 
-@pytest.mark.parametrize(
-    "return_rgb",
-    [
-        False,
-        True,
-    ],
-)
-def test_render_rgb_array(return_rgb):
-    render = Render(StubRender())
-    render.set_render_mode(mode="terminal")
+def _assert_rgb_array(render):
+    img = render.get_rgb_array()
+    assert img is not None
+    assert len(img.shape) == 3
+    assert img.shape[2] == 3
+    assert (img >= 0).all()
+    assert (img <= 255).all()
+    assert img.dtype == np.uint8
+
+
+def test_render_terminal():
+    render = Renderer(StubRender())
+    render.setup_render_mode(
+        requested_render_modes={"terminal"},
+        base_render_target="terminal_to_text",
+        interval=0,
+    )
 
     text = "StubRender\nAAA"
+    render.update_cache(text=text)
 
-    img = render.get_cached_rgb_array(return_rgb=return_rgb, text=text)
-    if return_rgb:
-        assert img is not None
-        assert len(img.shape) == 3
-        assert img.shape[2] == 3
-        assert (img >= 0).all()
-        assert (img <= 255).all()
-        assert img.dtype == np.uint8
-    else:
-        assert img is None
+    _assert_terminal_text(render, text)
+
+
+def test_render_terminal_to_image():
+    pytest.importorskip("pygame")
+    pytest.importorskip("PIL")
+    render = Renderer(StubRender())
+    render.setup_render_mode(
+        requested_render_modes={"terminal"},
+        base_render_target="terminal_to_rgb_array",
+        interval=0,
+    )
+
+    text = "StubRender\nAAA"
+    render.update_cache(text=text)
+
+    _assert_terminal_rgb_array(render)
+
+
+def test_render_rgb_array():
+    render = Renderer(StubRender())
+    render.setup_render_mode(
+        requested_render_modes={"rgb_array"},
+        base_render_target="rgb_array",
+        interval=0,
+    )
+
+    text = "StubRender\nAAA"
+    render.update_cache(text=text)
+
+    _assert_rgb_array(render)
 
 
 def test_render_window():
     if not is_available_pygame_video_device():
         pytest.skip("pygame.error: No available video device")
 
-    render = Render(StubRender())
-    render.set_render_mode(mode="window")
-
+    render = Renderer(StubRender())
+    render.setup_render_mode(
+        requested_render_modes={"rgb_array"},
+        base_render_target="window",
+        interval=0,
+    )
     for _ in range(10):
-        render.render(return_rgb=True)
+        render.render()
+
+
+def test_render_rl():
+    render = Renderer(StubRender())
+    render.setup_render_mode(
+        requested_render_modes={"terminal", "rgb_array"},
+        base_render_target="",
+        interval=0,
+    )
+    text = "StubRender\nAAA"
+    render.update_cache(text=text)
+
+    _assert_terminal_text(render, text)
+    _assert_terminal_rgb_array(render)
+    _assert_rgb_array(render)
